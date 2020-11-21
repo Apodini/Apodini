e6587da714578384ee8d420b4966fece3eea8e0b
Lorena Schlesinger <lorena.schlesinger@tum.de>
2020-11-21 22:41:24 +0100

    Add regression test for body property wrapper

--- Tests/ApodiniTests/RequestInjectableTests.swift
+++ Tests/ApodiniTests/RequestInjectableTests.swift
@@ -11,6 +11,43 @@ import Vapor
 
 final class RequestInjectableTests: XCTestCase {
     
+    func testBodyInjectable() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        
+        struct Message: Content {
+            var key: String
+        }
+               
+        struct SomeComponent: Component {
+            
+            @Body
+            var body: Message
+            
+            func handle() -> Message {
+                body
+            }
+        }
+        
+        let message: Message = Message(key: "test")
+        let messageJson = try JSONEncoder().encode(message)
+        let messageString = String(data: messageJson, encoding: .utf8)!
+        
+        var body = ByteBufferAllocator().buffer(capacity: 0)
+        body.writeString(messageString)
+        
+        app.routes.post("some") {req in
+            req.enterRequestContext(with: SomeComponent()) { component in
+                return component.handle().encodeResponse(for: req)
+            }
+        }
+        
+        try app.testable().test(.POST, "/some", body: body) { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, messageString)
+        }
+    }
+    
     func testQueryParameterInjectable() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
