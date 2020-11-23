f548a59d3745b2af57d47d30d976c9220f2dbbac
Alexander Collins <alexander.collins@tum.de>
2020-11-23 16:17:02 +0100

    Add missing documentation and fix swiftlint warnings

--- Sources/Apodini/Configurations/APNSConfiguration.swift
+++ Sources/Apodini/Configurations/APNSConfiguration.swift
@@ -11,12 +11,17 @@ import JWTKit
 
 
 public struct APNSConfiguration: Configuration {
-    
     private let authentication: APNSAuthentication
     private let topic: String
     private let environment: APNSwiftConfiguration.Environment
     
     
+    /// Creates a new configuration for APNS
+    ///
+    /// - Parameters:
+    ///     - authentication: The APNS authentication
+    ///     - topic: The Bundle identifier of your iOS application
+    ///     - environment: Specifies the APNS environment
     public init(_ authentication: APNSAuthentication, topic: String, environment: APNSwiftConfiguration.Environment) {
         self.authentication = authentication
         self.topic = topic
@@ -25,36 +30,48 @@ public struct APNSConfiguration: Configuration {
     
     public func configure(_ app: Application) {
         do {
-        switch authentication {
-        case .pem(let pemPath, let privateKeyPath, let pemPassword):
-            app.apns.configuration = try .init(
-                authenticationMethod: .tls(
-                    privateKeyPath: privateKeyPath ?? pemPath,
-                    pemPath: pemPath,
-                    pemPassword: pemPassword
-                ),
-                topic: topic,
-                environment: environment
-            )
-        case .p8(let path, let keyIdentifier, let teamIdentifier):
-            app.apns.configuration = try .init(
-                authenticationMethod: .jwt(
-                    key: .private(filePath: path),
-                    keyIdentifier: keyIdentifier,
-                    teamIdentifier: teamIdentifier
-                ),
-                topic: topic,
-                environment: environment
-            )
-        }
+            switch authentication {
+            case let .pem(pemPath, privateKeyPath, pemPassword):
+                app.apns.configuration = try .init(
+                    authenticationMethod: .tls(
+                        privateKeyPath: privateKeyPath ?? pemPath,
+                        pemPath: pemPath,
+                        pemPassword: pemPassword
+                    ),
+                    topic: topic,
+                    environment: environment
+                )
+            case let .p8(path, keyIdentifier, teamIdentifier):
+                app.apns.configuration = try .init(
+                    authenticationMethod: .jwt(
+                        key: .private(filePath: path),
+                        keyIdentifier: keyIdentifier,
+                        teamIdentifier: teamIdentifier
+                    ),
+                    topic: topic,
+                    environment: environment
+                )
+            }
         } catch {
             fatalError("Error setting up APNS")
         }
     }
 }
 
-
+/// An enum representing different options for APNS authentication
 public enum APNSAuthentication {
+    /// APNS authentication using the `.pem` file format
+    ///
+    /// - Parameters:
+    ///     - pemPath: The path to your `.pem` file
+    ///     - privateKeyPath: The path to your private key
+    ///     - pemPassword: The password for the private key
     case pem(pemPath: String, privateKeyPath: String? = nil, pemPassword: [UInt8]? = nil)
+    /// APNS authentication using the `.p8` file format
+    ///
+    /// - Parameters:
+    ///     - path: The path to your `.p8` file
+    ///     - keyIdentifier: The key ID of the `.p8` file
+    ///     - teamIdentifier: The team ID of your Apple Developer Account
     case p8(path: String, keyIdentifier: JWTKit.JWKIdentifier, teamIdentifier: String)
 }
