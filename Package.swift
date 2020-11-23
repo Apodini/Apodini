f1ef414713eaf69a1944aa157fe4b71bc3b6b958
Mathias Quintero <me@quintero.io>
2020-11-23 14:40:37 +0100

    Adds Tuple Component and expands maximum of components to 100

--- Package.swift
+++ Package.swift
@@ -14,14 +14,16 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.35.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.1")
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.1"),
+        .package(url: "https://github.com/nerdsupremacist/AssociatedTypeRequirementsKit.git", from: "0.2.0")
     ],
     targets: [
         .target(
             name: "Apodini",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "Fluent", package: "fluent")
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "AssociatedTypeRequirementsKit", package: "AssociatedTypeRequirementsKit")
             ]
         ),
         .testTarget(
---
+++ Sources/Apodini/Components/TupleComponent.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+public struct TupleComponent<T>: Component {
+    private let storage: T
+    #if DEBUG
+    let file: StaticString
+    let function: StaticString
+    #endif
+
+    #if DEBUG
+    init(_ storage: T, file: StaticString = #file, function: StaticString = #function) {
+        self.storage = storage
+        self.file = file
+        self.function = function
+    }
+    #else
+    init(_ storage: T) {
+        self.storage = storage
+    }
+    #endif
+}
+
+extension TupleComponent: Visitable {
+    func visit(_ visitor: SynaxTreeVisitor) {
+        visitor.enterCollectionItem()
+        let mirror = Mirror(reflecting: storage)
+        for (_, value) in mirror.children {
+            do {
+                try visitor.unsafeVisitAny(value)
+            } catch {
+                // Since init is internal & we only create Tuple Components in the Component Builder
+                // We know for a fact that unsafeVisit won't fail.
+                #if DEBUG
+                fatalError("Attemted to visit value that was not a component. It was instantiated from \(file):\(function): \(error)")
+                #else
+                fatalError(error)
+                #endif
+            }
+        }
+        visitor.exitCollectionItem()
+    }
+}
