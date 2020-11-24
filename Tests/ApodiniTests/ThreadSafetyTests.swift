3244e7688466f6ab00b1006fa6c6961d7becf09d
Max Obermeier <themomax@icloud.com>
2020-11-24 17:19:06 +0100

    simplyfy code of ThreadSafetyTests

--- Tests/ApodiniTests/ThreadSafetyTests.swift
+++ Tests/ApodiniTests/ThreadSafetyTests.swift
@@ -12,54 +12,6 @@ import Vapor
 import Fluent
 @testable import Apodini
 
-@propertyWrapper
-struct Atomic<Value> {
-    private var mutex = NSRecursiveLock()
-    private var _value: Value
-  
-    var wrappedValue: Value {
-        get {
-            return self.guard {
-                return _value
-            }
-        }
-        set {
-            self.guard {
-                _value = newValue
-            }
-        }
-    }
-    
-    init(wrappedValue value: Value) {
-        self._value = value
-    }
-}
-
-extension Atomic {
-    
-    func `guard`<T>(_ closure: () throws  -> T) rethrows -> T {
-        mutex.lock()
-        defer { mutex.unlock() }
-        return try closure()
-    }
-    
-}
-
-extension Atomic where Value: AdditiveArithmetic {
-
-    static func +=(left: inout Self, right: Value) {
-        left.guard {
-            left._value += right
-        }
-    }
-    
-    static func -=(left: inout Self, right: Value) {
-        left.guard {
-            left._value -= right
-        }
-    }
-}
-
 
 final class ThreadSafetyTests: ApodiniTests {
     
@@ -74,12 +26,12 @@ final class ThreadSafetyTests: ApodiniTests {
         
     }
     
-    @Atomic var testRequestInjectableUnlimitedConcurrencyCount: Int = 1000
-    
     func testRequestInjectableUnlimitedConcurrency() throws {
         let g = Greeter()
+        var count = 1000
+        var countMutex = NSLock()
         
-        DispatchQueue.concurrentPerform(iterations: testRequestInjectableUnlimitedConcurrencyCount) { _ in
+        DispatchQueue.concurrentPerform(iterations: count) { _ in
             
             let id = randomString(length: 40)
             let request = Request(application: app, collectedBody: ByteBuffer(string: id), on: app.eventLoopGroup.next())
@@ -93,21 +45,24 @@ final class ThreadSafetyTests: ApodiniTests {
                 let responseData = try XCTUnwrap(response.body.data)
                 
                 XCTAssert(String(data: responseData, encoding: .utf8) == id)
-                _testRequestInjectableUnlimitedConcurrencyCount -= 1
+                
+                countMutex.lock()
+                count -= 1
+                countMutex.unlock()
+                
             } catch {
                 XCTFail(error.localizedDescription)
             }
         }
         
-        XCTAssertEqual(testRequestInjectableUnlimitedConcurrencyCount, 0)
+        XCTAssertEqual(count, 0)
     }
-    
-    @Atomic var testRequestInjectableSingleThreadedCount: Int = 1000
     
     func testRequestInjectableSingleThreaded() throws {
         let g = Greeter()
+        var count = 1000
         
-        for _ in 0..<testRequestInjectableSingleThreadedCount {
+        for _ in 0..<count {
             let id = randomString(length: 40)
             let request = Request(application: app, collectedBody: ByteBuffer(string: id), on: app.eventLoopGroup.next())
             
@@ -120,13 +75,15 @@ final class ThreadSafetyTests: ApodiniTests {
                 let responseData = try XCTUnwrap(response.body.data)
                 
                 XCTAssert(String(data: responseData, encoding: .utf8) == id)
-                _testRequestInjectableSingleThreadedCount -= 1
+                
+                count -= 1
+                
             } catch {
                 XCTFail(error.localizedDescription)
             }
         }
         
-        XCTAssertEqual(testRequestInjectableSingleThreadedCount, 0)
+        XCTAssertEqual(count, 0)
     }
     
     private func randomString(length: Int) -> String {
