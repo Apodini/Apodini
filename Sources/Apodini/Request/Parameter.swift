6b83254ffa970d53a74d97e2e79fc340a1ac0ac4
Paul Schmiedmayer <paul.schmiedmayer@tum.de>
2020-11-25 01:15:54 +0100

    Add documentation to the @Parameter property wrapper

--- Sources/Apodini/Request/Parameter.swift
+++ Sources/Apodini/Request/Parameter.swift
@@ -9,12 +9,21 @@ import Vapor
 import Foundation
 
 
+/// The `@Parameter` property wrapper can be used to express input in different ways:
+/// * **Request Content**: Parameters can be part of the requests send to the web service such as the HTTP body or request content of an other protocol.
+/// * **Lightweight Parameter**: Some middleware types and protocols can expose parameters as lightweight parameters that can be part of a URI path such as query parameterst found in the URI of RESTful and OpenAPI based web APIs.
+/// * **Path Parameters**: Parameters can also be used to define the endpoint such as the URI path of the middleware types and protocols that support URI based multiplexing of requests.
 @propertyWrapper
 public class Parameter<Element: Codable> {
+    /// The `ParameterType` allows a developer to explicitly describe the type of parameter that should be used for the wrapped property
     public enum ParameterType {
+        /// The `.automatic` `ParameterType` allows Apodini to select the most suiting `ParameterType` based on the wrapped property, the `Component` and other context.
         case automatic
+        /// The `Parameter` is exposed as a request content. Parameters can be part of the requests send to the web service such as the HTTP body or request content of an other protocol.
         case content
+        /// The `Parameter` is exposed as a lightweight parameter. Some middleware types and protocols can expose parameters as lightweight parameters that can be part of a URI path such as query parameterst found in the URI of RESTful and OpenAPI based web APIs.
         case lightweight
+        /// The `Parameter` is exposed as a path parameter. Path parameters can be used to define the endpoint such as the URI path of the middleware types and protocols that support URI based multiplexing of requests.
         case path
     }
     
@@ -25,9 +34,10 @@ public class Parameter<Element: Codable> {
     private var element: Element?
     private var name: String?
     private var parameterType: ParameterType = .automatic
-    private var id: UUID = UUID()
+    private var id = UUID()
     
     
+    /// The value for the `@Parameter` as defined by the incoming request
     public var wrappedValue: Element {
         guard let element = element else {
             fatalError("You can only access the body while you handle a request")
@@ -36,6 +46,49 @@ public class Parameter<Element: Codable> {
         return element
     }
     
+    /// The `projectedValue` can be accessed using a `$` prefix before the property wrapped using the  `@Parameter` property wrapper.
+    /// It  can be used to pass a `.path` Parameter to children components.
+    ///
+    /// Example:
+    /// The `@Parameter` property used as a path parameter can also be defined outside the component as part of a Group that contains the `Compoent`.
+    /// ```swift
+    /// struct Bird: Identifiable {
+    ///     var id: Int
+    ///     var name: String
+    ///     var age: Int
+    /// }
+    ///
+    /// struct ExampleBirdComponent: Component {
+    ///     // As this was passed in from the outside this is automatically used as a path parameter
+    ///     @Parameter var birdID: Bird.ID
+    ///
+    ///     // ...
+    /// }
+    ///
+    /// struct ExampleNestComponent: Component {
+    ///     // As this was passed in from the outside this is automatically used as a path param
+    ///     @Parameter var birdID: Bird.ID
+    ///     // Exposed as a lightweight parameter
+    ///     @Parameter var nestName: String?
+    ///
+    ///     // ...
+    /// }
+    ///
+    /// struct TestWebService: WebService {
+    ///     @Parameter var birdID: Bird.ID
+    ///
+    ///     var content: some Component {
+    ///         Group("api", "birds", birdID) {
+    ///             ExampleBirdComponent(birdID: $birdID)
+    ///             Group("nests") {
+    ///                 ExampleNestComponent(birdID: $birdID)
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    ///
+    /// TestWebService.main()
+    /// ```
     public var projectedValue: Parameter {
         guard parameterType == .path || parameterType == .automatic else {
             preconditionFailure("Only `.path` or `.automatic` parameters are allowed to be passed to a `Component`.")
@@ -44,10 +97,14 @@ public class Parameter<Element: Codable> {
         return Parameter(name: self.name, parameterType: .path, id: self.id)
     }
     
+    /// - Parameter parameterType: The `ParameterType` that explicitly describes the type of parameter that should be used for the wrapped property
     public init(_ parameterType: ParameterType = .automatic) {
         self.parameterType = parameterType
     }
     
+    /// - Parameters:
+    ///   - name: The name used to describe the wrapped property for in the web APIs
+    ///   - parameterType: The `ParameterType` that explicitly describes the type of parameter that should be used for the wrapped property
     public init(_ name: String, _ parameterType: ParameterType = .automatic) {
         self.name = name
         self.parameterType = parameterType
