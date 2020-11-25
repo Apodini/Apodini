6810fb8959440abd2ad4ffe1a0a2ff87398fd79a
Moritz Schüll <moritzschuell@live.de>
2020-11-25 09:54:43 +0100

    Allow user to specify certificate and private key via command line arguments

--- Package.swift
+++ Package.swift
@@ -14,14 +14,16 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.35.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.1")
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0")
     ],
     targets: [
         .target(
             name: "Apodini",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "Fluent", package: "fluent")
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]
         ),
         .testTarget(
--- Sources/Apodini/WebService.swift
+++ Sources/Apodini/WebService.swift
@@ -1,11 +1,14 @@
 //
-//  File.swift
+//  WebService.swift
 //  
 //
 //  Created by Paul Schmiedmayer on 7/6/20.
 //
 
 import Vapor
+import ArgumentParser
+import NIOSSL
+
 
 /// Each Apodini program conists of a `WebService`component that is used to describe the Web API of the Web Service
 public protocol WebService: Component {
@@ -20,27 +23,7 @@ public protocol WebService: Component {
 extension WebService {
     /// This function is exectured to start up an Apodini `WebService`
     public static func main() {
-        do {
-            var env = try Environment.detect()
-            try LoggingSystem.bootstrap(from: &env)
-            let app = Application(env)
-            
-            let webService = Self()
-            
-            webService.register(
-                RESTSemanticModelBuilder(app),
-                GraphQLSemanticModelBuilder(app),
-                GRPCSemanticModelBuilder(app),
-                WebSocketSemanticModelBuilder(app)
-            )
-            
-            defer {
-                app.shutdown()
-            }
-            try app.run()
-        } catch {
-            print(error)
-        }
+        Command<Self>.main()
     }
     
     
@@ -69,5 +52,56 @@ extension WebService {
         Group {
             content
         }.visit(visitor)
+    }
+}
+
+
+private struct Command<W: WebService>: ParsableCommand {
+    /// ArguementParser currently supports the following options:
+    ///     --cert  File-path to a .pem file for the certificate to use
+    ///     --key   File-path ot a .pem file for the key to use
+    ///
+    /// Only if both --cert and --key are set, HTTP/1 & HTTP/2 and TLS will be enabled. Otherwise, only HTTP/1 will be enabled, without TLS.
+    @ArgumentParser.Option(help: "Path to a certificate .pem file")
+    var cert: String?
+    @ArgumentParser.Option(help: "Path to a key .pem file")
+    var key: String?
+
+
+    private func configureHttp2(for app: Application) throws {
+        if let cert = cert, let key = key {
+            let certFile = try Data(contentsOf: URL(fileURLWithPath: cert))
+            let keyFile = try Data(contentsOf: URL(fileURLWithPath: key))
+            let certificates = try NIOSSLCertificate.fromPEMBytes([UInt8](certFile))
+            let privateKey = try NIOSSLPrivateKey.init(bytes: [UInt8](keyFile), format: .pem)
+            app.http.server.configuration.supportVersions = [.one, .two]
+            app.http.server.configuration.tlsConfiguration = .forServer(certificateChain: certificates.map { .certificate($0) }, privateKey: .privateKey(privateKey))
+        }
+    }
+
+    func run() throws {
+        do {
+            let environmentName = try Environment.detect().name
+            var env = Environment(name: environmentName, arguments: ["vapor"])
+            try LoggingSystem.bootstrap(from: &env)
+            let app = Application(env)
+
+            let webService = W()
+            try configureHttp2(for: app)
+
+            webService.register(
+                RESTSemanticModelBuilder(app),
+                GraphQLSemanticModelBuilder(app),
+                GRPCSemanticModelBuilder(app),
+                WebSocketSemanticModelBuilder(app)
+            )
+
+            defer {
+                app.shutdown()
+            }
+            try app.run()
+        } catch {
+            print(error)
+        }
     }
 }
