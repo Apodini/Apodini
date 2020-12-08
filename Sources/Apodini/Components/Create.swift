f3dfeb3356a1d548f6bbf797936ff29602e67c96
Felix Desiderato <felix.desiderato@tum.de>
2020-12-08 09:27:21 +0100

    moved query builder to separate file

--- Sources/Apodini/Components/Create.swift
+++ Sources/Apodini/Components/Create.swift
@@ -50,7 +50,7 @@ public struct Get<T: DatabaseModel>: Component where T.IDValue: LosslessStringCo
     var queryString: String
     
     public func handle() -> EventLoopFuture<[T]> {
-        let queryBuilder = MyQueryBuilder(type: T.self, queryString: queryString)
+        let queryBuilder = Apodini.QueryBuilder(type: T.self, queryString: queryString)
         return queryBuilder.execute(on: database)
     }
     public init() {}
@@ -76,7 +76,6 @@ public struct Update<T: DatabaseModel>: Component where T.IDValue: LosslessStrin
         Request called on: %@
         DatabaseModel used: %@
         """, request.url.string, object.description)
-        T.query(on: database).filter(\., <#T##method: DatabaseQuery.Filter.Method##DatabaseQuery.Filter.Method#>, <#T##value: Decodable & Encodable##Decodable & Encodable#>)
         return T.find(id, on: database).flatMapThrowing({ model -> T in
             model?.update(object)
             return model!
@@ -111,55 +110,5 @@ public struct Delete<T: DatabaseModel>: Component {
     public init() {}
 }
 
-public struct MyQueryBuilder<Model: DatabaseModel> {
-    
-    private let type: Model.Type
-    private let queryString: String
 
-    private var fieldKeys: [FieldKey] {
-        return type.keys
-    }
-
-    private var parameters: [FieldKey: String] {
-        var _parameters: [FieldKey: String] = [:]
-        let queryParts = queryString.split(separator: "&").map({ String($0) })
-        for (index,part) in queryParts.enumerated() {
-            let queryParameters = part.split(separator: "=").map({ String($0) })
-            guard queryParameters.count == 2 else { fatalError("invalid query") }
-            let key = queryParameters[0]
-            let value = queryParameters[1]
-            guard let fieldKey = fieldKeys.first(where: { $0.description == key }) else { continue }
-            _parameters[fieldKey] = value
-        }
-        return _parameters
-    }
-
-    init(type: Model.Type, queryString: String) {
-        self.type = type
-        self.queryString = queryString
-    }
-    
-    
-    public func execute(on database: Fluent.Database) -> EventLoopFuture<[Model]> {
-        
-        func accumulateFilters(queryBuilder: inout QueryBuilder<Model>, key: FieldKey, value: String, method: DatabaseQuery.Filter.Method) {
-            if let intValue = Int(value) {
-                queryBuilder.filter(key, method, intValue)
-            } else {
-                queryBuilder.filter(key, method, value)
-            }
-        }
-        
-        var queryBuilder = Model.query(on: database)
-        for (key, value) in parameters {
-            accumulateFilters(queryBuilder: &queryBuilder, key: key, value: value, method: .equal)
-        }
-        return queryBuilder.all()
-    }
-    
-    @discardableResult
-    public static func fieldKeys<Model: DatabaseModel>(for type: Model.Type) -> [FieldKey] {
-        return type.keys
-    }
-}
 
