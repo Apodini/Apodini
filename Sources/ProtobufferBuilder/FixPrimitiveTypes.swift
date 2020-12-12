4b6248f30ad7ecfdda851e9cf3bb6bda67d49826
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-12-12 23:54:05 +0100

    rm import

--- Sources/ProtobufferBuilder/FixPrimitiveTypes.swift
+++ Sources/ProtobufferBuilder/FixPrimitiveTypes.swift
@@ -5,8 +5,6 @@
 //  Created by Nityananda on 12.12.20.
 //
 
-import Foundation
-
 func fixPrimitiveTypes(_ node: Node<EnrichedInfo>) throws -> Tree<EnrichedInfo> {
     guard ParticularType(node.value.typeInfo.type).isPrimitive else {
         return node
