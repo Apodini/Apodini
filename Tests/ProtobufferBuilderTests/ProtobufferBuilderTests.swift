f0bd235bfa6ebfba690181d19865fe5cf6d99d01
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-12-14 11:55:58 +0100

    Create ProtobufferBuilderInternalTests

--- Tests/ProtobufferBuilderTests/ProtobufferBuilderTests.swift
+++ Tests/ProtobufferBuilderTests/ProtobufferBuilderTests.swift
@@ -2,123 +2,7 @@ import XCTest
 import Apodini
 import ProtobufferBuilder
 
-// MARK: - Test Supported Types
-
-final class ProtobufferBuilderTests: XCTestCase {
-    func testVoid() throws {
-        XCTAssertNoThrow(try buildMessage(Void.self))
-    }
-    
-    func testTuple() throws {
-        XCTAssertThrowsError(try buildMessage((Int, String).self))
-    }
-    
-    func testTriple() throws {
-        XCTAssertThrowsError(try buildMessage((Int, String, Void).self))
-    }
-    
-    func testClass() throws {
-        class Person {
-            var name: String = ""
-            var age: Int = 0
-        }
-        
-        XCTAssertNoThrow(try buildMessage(Person.self))
-    }
-    
-    func testEnum() throws {
-        enum JSON {
-            case null
-            case bool(Bool)
-            case number(Double)
-            case string(String)
-            case array([JSON])
-            case object([String: JSON])
-        }
-        
-        XCTAssertThrowsError(try buildMessage(JSON.self))
-    }
-}
-
-// MARK: - Test Output
-
-extension ProtobufferBuilderTests {
-    func testScalarType() throws {
-        let expected = """
-            syntax = "proto3";
-
-            message StringMessage {
-              string value = 1;
-            }
-            """
-        
-        XCTAssertEqual(try buildMessage(String.self), expected)
-    }
-    
-    func testTypeOneLevelDeep() throws {
-        struct Location {
-            let latitude: UInt32
-            let longitude: UInt32
-        }
-        
-        let expected = """
-            syntax = "proto3";
-
-            message LocationMessage {
-              uint32 latitude = 1;
-              uint32 longitude = 2;
-            }
-            """
-        
-        XCTAssertEqual(try buildMessage(Location.self), expected)
-    }
-    
-    func testTypeTwoLevelsDeep() throws {
-        struct Account {
-            let transactions: [Transaction]
-        }
-        
-        struct Transaction {
-            let amount: Int32
-        }
-        
-        let expected = """
-            syntax = "proto3";
-
-            message AccountMessage {
-              repeated TransactionMessage transactions = 1;
-            }
-
-            message TransactionMessage {
-              int32 amount = 1;
-            }
-            """
-        
-        XCTAssertEqual(try buildMessage(Account.self), expected)
-    }
-    
-    func testRecursiveType() throws {
-        return
-        
-        #warning("TODO: Enforce DAG")
-        
-        struct Node<T> {
-            let value: T
-            let children: [Node]
-        }
-        
-        let expected = """
-            syntax = "proto3";
-
-            message NodeOfInt64Message {
-              int64 value = 1;
-              repeated NodeOfInt64Message children = 2;
-            }
-            """
-        
-        XCTAssertEqual(try buildMessage(Node<Int64>.self), expected)
-    }
-}
+final class ProtobufferBuilderTests: XCTestCase {}
 
 // MARK: - Test Components
 
@@ -158,21 +42,6 @@ extension ProtobufferBuilderTests {
 }
 
 // MARK: - Private
-
-@discardableResult
-private func buildMessage<T>(_ type: T.Type) throws -> String {
-    let builder = ProtobufferBuilder()
-    try builder.addMessage(of: type)
-    let description = builder.description
-    
-    print("""
-        ----
-        \(description)
-        ----
-        """)
-    
-    return description
-}
 
 @discardableResult
 private func buildService<T: Component>(_ type: T.Type) throws -> String {
