0811646d1c87cb51b4817402a2228d2968e201fb
Tim Gymnich <tgymnich@icloud.com>
2020-12-19 19:19:59 +0100

    Replace ResponseEncodable with Encodable (#61)
    * replace ResponseEncodable with Encodable
    
    * add Request protocol
    
    * added AnyEncodable
    
    * remove vapor imports

--- Sources/Apodini/Semantic Model Builder/Context.swift
+++ Sources/Apodini/Semantic Model Builder/Context.swift
@@ -5,8 +5,6 @@
 //  Created by Paul Schmiedmayer on 11/3/20.
 //
 
-import Vapor
-
 
 class Context {
     private let contextNode: ContextNode
@@ -21,29 +19,4 @@ class Context {
         contextNode.getContextValue(for: contextKey)
     }
 
-    @available(*, deprecated, message: "To be replaced by the `requestHandlerBuilder` in the Endpoint model. See SharedSemanticModelBuilder")
-    func createRequestHandler<C: Component>(withComponent component: C, using decoder: SemanticModelBuilder)
-    -> (Vapor.Request) -> EventLoopFuture<Vapor.Response> {
-        { (request: Vapor.Request) in
-            let guardEventLoopFutures = self.contextNode.getContextValue(for: GuardContextKey.self)
-                .map { requestGuard in
-                    request.enterRequestContext(with: requestGuard(), using: decoder) { requestGuard in
-                        requestGuard.executeGuardCheck(on: request)
-                    }
-                }
-            return EventLoopFuture<Void>
-                .whenAllSucceed(guardEventLoopFutures, on: request.eventLoop)
-                .flatMap { _ in
-                    request.enterRequestContext(with: component, using: decoder) { component in
-                        var response: ResponseEncodable = component.handle()
-                        for responseTransformer in self.contextNode.getContextValue(for: ResponseContextKey.self) {
-                            response = request.enterRequestContext(with: responseTransformer()) { responseTransformer in
-                                responseTransformer.transform(response: response)
-                            }
-                        }
-                        return response.encodeResponse(for: request)
-                    }
-                }
-        }
-    }
 }
