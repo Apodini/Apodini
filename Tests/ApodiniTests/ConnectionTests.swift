25c2855f5365ebda24b1d7d1b6ce1d1d6e10e3d9
Moritz Schüll <moritzschuell@live.de>
2020-12-21 16:02:43 +0100

    Add test for connection injection

---
+++ Tests/ApodiniTests/ConnectionTests.swift
@@ -0,0 +1,52 @@
+//
+//  ConnectionTests.swift
+//  
+//
+//  Created by Moritz Schüll on 21.12.20.
+//
+
+import XCTest
+import Vapor
+@testable import Apodini
+
+final class ConnectionTests: XCTestCase {
+    struct TestComponent: Component {
+        @ClientConnection
+        var connection: Connection
+
+        var endMessage: String
+        var openMessage: String
+
+        public func handle() -> Action<String> {
+            switch connection.state {
+            case .open:
+                return .send(openMessage)
+            case .end:
+                return .final(endMessage)
+            }
+        }
+    }
+
+    func testConnectionInjection() {
+        let endMessage = "End"
+        let openMessage = "Open"
+
+        let testComponent = TestComponent(endMessage: endMessage, openMessage: openMessage)
+
+        var connection = Connection(state: .open)
+        let returnedActionWithOpen = testComponent.withConnection(connection).handle()
+        if case let .send(returnedMessageWithOpen) = returnedActionWithOpen {
+            XCTAssertEqual(returnedMessageWithOpen, openMessage)
+        } else {
+            XCTFail("Expected Action send(\(openMessage)), but was \(returnedActionWithOpen)")
+        }
+
+        connection.state = .end
+        let returnedActionWithEnd = testComponent.withConnection(connection).handle()
+        if case let .final(returnedMessageWithEnd) = returnedActionWithEnd {
+            XCTAssertEqual(returnedMessageWithEnd, endMessage)
+        } else {
+            XCTFail("Expected Action final(\(openMessage)), but was \(returnedActionWithEnd)")
+        }
+    }
+}
