ace873b04a00ef4a769c3f2aaa6197afade3672a
Alexander Collins <alexander.collins@tum.de>
2020-12-22 01:21:49 +0100

    Extend test cases

--- Tests/ApodiniTests/NotificationCenterTests.swift
+++ Tests/ApodiniTests/NotificationCenterTests.swift
@@ -11,38 +11,47 @@ import Vapor
 import Fluent
 @testable import Apodini
 
-
 final class NotificationCenterTests: ApodiniTests {
+    var notificationCenter = NotificationCenter.shared
+    
     override func setUp() {
         super.setUp()
-        NotificationCenter.shared.application = self.app
+        notificationCenter = EnvironmentValues.shared.notificationCenter
+        notificationCenter.application = self.app
     }
 
     func testDeviceRegistration() throws {
         let topic = "test"
         let device = Device(id: "123", type: .apns, topics: [topic])
+        let device2 = Device(id: "777", type: .fcm, topics: [topic])
+        let device3 = Device(id: "888", type: .fcm)
         
-        NotificationCenter.shared.register(device: device)
+        try notificationCenter.register(device: device).wait()
+        try notificationCenter.register(device: device2).wait()
+        try notificationCenter.register(device: device3).wait()
         
-        let devices = try NotificationCenter.shared.getAllDevices().wait()
+        let devices = try notificationCenter.getAllDevices().wait()
         let savedTopic = try Topic.query(on: app.db).filter(\.$name == topic).first().wait()
-        let devicesOfTopic = try NotificationCenter.shared.getDevices(of: topic).wait()
+        let devicesOfTopic = try notificationCenter.getDevices(of: topic).wait()
 
         XCTAssert(devices.contains(device))
+        XCTAssert(devices.contains(device2))
+        XCTAssert(devices.contains(device3))
         XCTAssertNotNil(savedTopic)
         XCTAssert(devicesOfTopic.contains(device))
+        XCTAssert(devicesOfTopic.contains(device2))
     }
     
     func testDeviceDeletion() throws {
         let topic = "test"
         let device = Device(id: "321", type: .apns, topics: [topic])
         
-        NotificationCenter.shared.register(device: device)
+        notificationCenter.register(device: device)
 
-        try NotificationCenter.shared.delete(device: device).wait()
-        let devices = try NotificationCenter.shared.getAllDevices().wait()
+        try notificationCenter.delete(device: device).wait()
+        let devices = try notificationCenter.getAllDevices().wait()
             
-        let devicesTopic = try NotificationCenter.shared.getDevices(of: topic).wait()
+        let devicesTopic = try notificationCenter.getDevices(of: topic).wait()
         
         XCTAssertFalse(devices.contains(device))
         XCTAssertFalse(devicesTopic.contains(device))
@@ -51,21 +60,27 @@ final class NotificationCenterTests: ApodiniTests {
     func testAddingTopicToDevice() throws {
         let topics = ["topic1", "topic2", "topic3"]
         let device = Device(id: "999", type: .apns)
+        let device2 = Device(id: "000", type: .apns)
         
-        try NotificationCenter.shared.register(device: device).wait()
-        try NotificationCenter.shared.addTopics("topic1", "topic2", "topic3", to: device).wait()
-        let deviceReturn = try NotificationCenter.shared.getDevice(id: device.id).wait()
+        try notificationCenter.register(device: device).wait()
+        try notificationCenter.register(device: device2).wait()
+        try notificationCenter.addTopics("topic1", "topic2", "topic3", to: device).wait()
+        try notificationCenter.addTopics("topic1", to: device2).wait()
+        let deviceReturn = try notificationCenter.getDevice(id: device.id).wait()
         let deviceTopics = deviceReturn.topics ?? []
+        let devicesTopic = try notificationCenter.getDevices(of: "topic1").wait()
 
         XCTAssert(deviceTopics.sorted() == topics.sorted())
+        XCTAssert(devicesTopic.contains(device))
+        XCTAssert(devicesTopic.contains(device2))
     }
     
     func testRemovingTopicFromDevice() throws {
         let topic = "topic"
         let device = Device(id: "789", type: .apns, topics: [topic])
         
-        try NotificationCenter.shared.register(device: device).wait()
-        try NotificationCenter.shared.remove(topic: topic, from: device).wait()
+        try notificationCenter.register(device: device).wait()
+        try notificationCenter.remove(topic: topic, from: device).wait()
         let deviceReturn = try DeviceDatabaseModel
             .query(on: app.db)
             .filter(\.$id == device.id)
@@ -92,8 +107,8 @@ final class NotificationCenterTests: ApodiniTests {
         
         try devices.create(on: app.db).wait()
         
-        let retrievedAPNS = try NotificationCenter.shared.getAPNSDevices().wait()
-        let retrievedFCM = try NotificationCenter.shared.getFCMDevices().wait()
+        let retrievedAPNS = try notificationCenter.getAPNSDevices().wait()
+        let retrievedFCM = try notificationCenter.getFCMDevices().wait()
         
         XCTAssert(retrievedAPNS == apnsDevices.map { Device(id: $0.id ?? "", type: $0.type) })
         XCTAssert(retrievedFCM == fcmDevices.map { Device(id: $0.id ?? "", type: $0.type) })
