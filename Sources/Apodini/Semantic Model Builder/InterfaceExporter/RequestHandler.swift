aab5e47b837b53c11be73c1ff025949bbfefd72c
Andi <andi.bauer@tum.de>
2021-01-02 00:44:25 +0100

    Adding basic HATEOAS support to the REST exporter (#86)
    * Adding basic HATEOAS support to the REST exporter
    
    * Adding a test case covering rest request handling
    
    * Remove intermediate database solution

--- Sources/Apodini/Semantic Model Builder/InterfaceExporter/RequestHandler.swift
+++ Sources/Apodini/Semantic Model Builder/InterfaceExporter/RequestHandler.swift
@@ -7,7 +7,7 @@ import protocol NIO.EventLoop
 import protocol FluentKit.Database
 
 class EndpointRequestHandler<I: InterfaceExporter> {
-    func callAsFunction(request: I.ExporterRequest, eventLoop: EventLoop, database: Database? = nil) -> EventLoopFuture<Encodable> {
+    func callAsFunction(request: I.ExporterRequest, eventLoop: EventLoop) -> EventLoopFuture<Encodable> {
         // We are doing nothing here. Everything is handled in InternalEndpointRequestHandler
         fatalError("EndpointRequestHandler.handleRequest() was not overridden. EndpointRequestHandler must not be created manually!")
     }
@@ -28,21 +28,8 @@ class InternalEndpointRequestHandler<I: InterfaceExporter, H: Handler>: Endpoint
         self.exporter = exporter
     }
 
-    override func callAsFunction(
-        request exporterRequest: I.ExporterRequest,
-        eventLoop: EventLoop,
-        database: Database? = nil
-    ) -> EventLoopFuture<Encodable> {
-        let databaseClosure: (() -> Database)?
-        if let database = database {
-            databaseClosure = { database }
-        } else if let requestWithDatabase = exporterRequest as? WithDatabase {
-            databaseClosure = requestWithDatabase.database
-        } else {
-            databaseClosure = nil
-        }
-
-        let request = ApodiniRequest(for: exporter, with: exporterRequest, on: endpoint, running: eventLoop, database: databaseClosure)
+    override func callAsFunction(request exporterRequest: I.ExporterRequest, eventLoop: EventLoop) -> EventLoopFuture<Encodable> {
+        let request = ApodiniRequest(for: exporter, with: exporterRequest, on: endpoint, running: eventLoop)
 
         let guardEventLoopFutures = endpoint.guards.map { guardClosure in
             request.enterRequestContext(with: guardClosure()) { requestGuard in
--- Sources/Apodini/Semantic Model Builder/REST/RESTInterfaceExporter.swift
+++ Sources/Apodini/Semantic Model Builder/REST/RESTInterfaceExporter.swift
@@ -4,7 +4,6 @@
 
 import Foundation
 @_implementationOnly import Vapor
-import protocol FluentKit.Database
 
 struct RESTPathBuilder: PathBuilder {
     private var pathComponents: [Vapor.PathComponent] = []
@@ -61,12 +60,6 @@ extension Operation {
     }
 }
 
-extension Vapor.Request: ExporterRequest, WithEventLoop, WithDatabase {
-    var database: () -> Database {{
-        self.db
-    }}
-}
-
 class RESTInterfaceExporter: InterfaceExporter {
     let app: Application
 
@@ -83,24 +76,8 @@ class RESTInterfaceExporter: InterfaceExporter {
         let exportedParameterNames = endpoint.exportParameters(on: self)
 
         let requestHandler = endpoint.createRequestHandler(for: self)
-
-        routesBuilder.on(operation.httpMethod, []) { (request: Vapor.Request) -> EventLoopFuture<Vapor.Response> in
-            let responseFuture = requestHandler(request: request)
-
-            return responseFuture.flatMap { encodable in
-                let jsonEncoder = JSONEncoder()
-                jsonEncoder.outputFormatting = [.withoutEscapingSlashes, .prettyPrinted]
-                #warning("We may remove JSONEncoder .prettyPrinted in production or make it configurable in some way")
-
-                let response = Response()
-                do {
-                    try response.content.encode(AnyEncodable(value: encodable), using: jsonEncoder)
-                } catch {
-                    return request.eventLoop.makeFailedFuture(error)
-                }
-                return request.eventLoop.makeSucceededFuture(response)
-            }
-        }
+        let endpointHandler = RESTEndpointHandler(for: endpoint, with: requestHandler)
+        endpointHandler.register(at: routesBuilder, with: operation)
 
         app.logger.info("Exported '\(operation.httpMethod.rawValue) \(pathBuilder.pathDescription)' with parameters: \(exportedParameterNames)")
 
