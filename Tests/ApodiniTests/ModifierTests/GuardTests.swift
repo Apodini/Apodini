d3f03dbd8e11e86081a6c38e9f3babce57722b70
Paul Schmiedmayer <paul.schmiedmayer@tum.de>
2021-01-02 17:26:02 +0100

    Add a unit test for `Guard`s

--- Tests/ApodiniTests/ModifierTests/GuardTests.swift
+++ Tests/ApodiniTests/ModifierTests/GuardTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import XCTVapor
+import protocol Fluent.Database
 @testable import Apodini
 
 
@@ -14,17 +15,58 @@ final class GuardTests: ApodiniTests {
     private static var guardExpectation: XCTestExpectation?
     
     
-    private struct TestGuard: SyncGuard {
-        func check() {
-            guard let guardExpectation = GuardTests.guardExpectation else {
-                fatalError("The test expectation must be set before testing `TestGuard`")
+    func testSyncGuard() throws {
+        struct TestSyncGuard: SyncGuard {
+            func check() {
+                guard let guardExpectation = GuardTests.guardExpectation else {
+                    fatalError("The test expectation must be set before testing `TestGuard`")
+                }
+                guardExpectation.fulfill()
             }
-            guardExpectation.fulfill()
+        }
+        
+        GuardTests.guardExpectation = self.expectation(description: "Guard is exectured")
+        
+        struct TestWebService: WebService {
+            var version = Version(prefix: "v", major: 2, minor: 1, patch: 0)
+            
+            var content: some Component {
+                Text("Hello")
+                    .guard(TestSyncGuard())
+            }
+        }
+        
+        TestWebService.main(app: app)
+        
+        
+        try app.test(.GET, "/v2/") { res in
+            XCTAssertEqual(res.status, .ok)
+            
+            struct Content: Decodable {
+                let data: String
+            }
+            
+            let content = try res.content.decode(Content.self)
+            XCTAssert(content.data == "Hello")
+            waitForExpectations(timeout: 0, handler: nil)
         }
     }
     
-    
-    func testSyncGuard() throws {
+    func testGuard() throws {
+        struct TestGuard: Guard {
+            @Apodini.Environment(\.database)
+            var database: Database
+            
+            func check() -> EventLoopFuture<Void> {
+                guard let guardExpectation = GuardTests.guardExpectation else {
+                    fatalError("The test expectation must be set before testing `TestGuard`")
+                }
+                guardExpectation.fulfill()
+                
+                return database.eventLoop.makeSucceededFuture(Void())
+            }
+        }
+        
         GuardTests.guardExpectation = self.expectation(description: "Guard is exectured")
         
         struct TestWebService: WebService {
