8bb58dc45777b6d20091a13300ad21ac29ad2191
Paul Schmiedmayer <paul.schmiedmayer@tum.de>
2021-01-03 01:16:12 +0100

    Fix the ActionTests on Linux environments

--- Tests/ApodiniTests/PropertiesTests/ActionTests.swift
+++ Tests/ApodiniTests/PropertiesTests/ActionTests.swift
@@ -14,6 +14,30 @@ final class ActionTests: XCTestCase {
     
     
     struct TextEncoder: Encoder {
+        struct TestEncoderSingleValueContainer: SingleValueEncodingContainer {
+            var codingPath: [CodingKey]
+            
+            mutating func encodeNil() throws { }
+            mutating func encode(_ value: Bool) throws { }
+            mutating func encode(_ value: String) throws {
+                XCTAssertEqual("Paul", value)
+                ActionTests.actionExpectation?.fulfill()
+            }
+            mutating func encode(_ value: Double) throws { }
+            mutating func encode(_ value: Float) throws { }
+            mutating func encode(_ value: Int) throws { }
+            mutating func encode(_ value: Int8) throws { }
+            mutating func encode(_ value: Int16) throws { }
+            mutating func encode(_ value: Int32) throws { }
+            mutating func encode(_ value: Int64) throws { }
+            mutating func encode(_ value: UInt) throws { }
+            mutating func encode(_ value: UInt8) throws { }
+            mutating func encode(_ value: UInt16) throws { }
+            mutating func encode(_ value: UInt32) throws { }
+            mutating func encode(_ value: UInt64) throws { }
+            mutating func encode<T>(_ value: T) throws where T : Encodable { }
+        }
+        
         var codingPath: [CodingKey] = []
         var userInfo: [CodingUserInfoKey: Any] = [:]
         
@@ -26,8 +50,7 @@ final class ActionTests: XCTestCase {
         }
         
         func singleValueContainer() -> SingleValueEncodingContainer {
-            ActionTests.actionExpectation?.fulfill()
-            fatalError("Decoding started: SingleValueContainer")
+            TestEncoderSingleValueContainer(codingPath: codingPath)
         }
     }
     
@@ -40,12 +63,12 @@ final class ActionTests: XCTestCase {
         
         ActionTests.actionExpectation = self.expectation(description: "Decoding Started")
         let sendAction = Action.send("Paul")
-        XCTAssertRuntimeFailure(try? sendAction.encode(to: TextEncoder()))
+        try? sendAction.encode(to: TextEncoder())
         waitForExpectations(timeout: 0, handler: nil)
         
         ActionTests.actionExpectation = self.expectation(description: "Decoding Started")
         let finalAction = Action.final("Paul")
-        XCTAssertRuntimeFailure(try? finalAction.encode(to: TextEncoder()))
+        try? finalAction.encode(to: TextEncoder())
         waitForExpectations(timeout: 0, handler: nil)
     }
 }
