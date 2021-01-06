8bd8d20847967433acd2aeee37f4ea662f6b2acc
Max Obermeier <themomax@icloud.com>
2021-01-06 10:27:36 +0100

    adapt WS exporter: transfer state-handling to shared model

--- Sources/Apodini/Semantic Model Builder/WebSocket/WebSocketInterfaceExporter.swift
+++ Sources/Apodini/Semantic Model Builder/WebSocket/WebSocketInterfaceExporter.swift
@@ -11,7 +11,38 @@
 import OpenCombine
 @_implementationOnly import Runtime
 
-extension SomeInput: ExporterRequest, Reducible { }
+extension SomeInput: ExporterRequest, Reducible {
+    func reduce(to new: SomeInput) -> SomeInput {
+        var newParameters: [String: InputParameter] = [:]
+        for (name, value) in new.parameters {
+            if let reducible = self.parameters[name] as? ReducibleParameter {
+                newParameters[name] = reducible.reduce(to: value)
+            } else {
+                newParameters[name] = value
+            }
+        }
+        return SomeInput(parameters: newParameters)
+    }
+}
+
+private protocol ReducibleParameter {
+    func reduce(to new: InputParameter) -> InputParameter
+}
+
+extension BasicInputParameter: ReducibleParameter {
+    func reduce(to new: InputParameter) -> InputParameter {
+        if let newParameter = new as? Self {
+            switch newParameter.value {
+            case .some:
+                return new
+            case .none:
+                return self
+            }
+        } else {
+            return new
+        }
+    }
+}
 
 
 class WebSocketInterfaceExporter: InterfaceExporter {
@@ -34,16 +65,16 @@ class WebSocketInterfaceExporter: InterfaceExporter {
     func export<C: Component>(_ endpoint: Endpoint<C>) {
         let inputParameters: [(String, InputParameter)] = endpoint.exportParameters(on: self)
         
-        let defaultInput = SomeInput(parameters: inputParameters.reduce(into: [String: InputParameter](), { result, parameter in
+        let emptyInput = SomeInput(parameters: inputParameters.reduce(into: [String: InputParameter](), { result, parameter in
             result[parameter.0] = parameter.1
         }))
 
+        var latestInput = emptyInput
+        
         self.router.register({(input: AnyPublisher<SomeInput, Never>, eventLoop: EventLoop, _: Database?) -> (
                     defaultInput: SomeInput,
                     output: AnyPublisher<Message<AnyEncodable>, Error>
                 ) in
-            let defaultInput = defaultInput
-            
             var context = endpoint.createConnectionContext(for: self)
 
             let output: PassthroughSubject<Message<AnyEncodable>, Error> = PassthroughSubject()
@@ -53,23 +84,19 @@ class WebSocketInterfaceExporter: InterfaceExporter {
                 This can lead to undefined behavior on parallel requests.
             """)
             inputCancellable = input.sink(receiveCompletion: { completion in
-                inputCancellable?.cancel()
-
-                #warning("The WebSocketExporter does not handle 'Action' yet. Using anything but '.send' or '.final' results in a server-crash.")
-                output.send(completion: .finished)
-            }, receiveValue: { inputValue in
-                context.handle(request: inputValue, eventLoop: eventLoop).whenComplete { result in
-                    switch result {
-                    case .success(let response):
-                        output.send(.message(AnyEncodable(value: response)))
-                    case .failure(let error):
-                        output.send(.error(error))
-                    }
+                switch completion {
+                case .finished:
+                    Self.handleInputCompletion(with: latestInput, using: &context, on: eventLoop, output: output)
                 }
+                
+                inputCancellable?.cancel()
+            }, receiveValue: { inputValue in
+                latestInput = inputValue
+                Self.handleRegularInput(with: latestInput, using: &context, on: eventLoop, output: output)
             })
 
 
-            return (defaultInput: defaultInput, output: output.eraseToAnyPublisher())
+            return (defaultInput: emptyInput, output: output.eraseToAnyPublisher())
         }, on: WebSocketPathBuilder(endpoint.absolutePath).pathIdentifier)
     }
     
@@ -77,7 +104,7 @@ class WebSocketInterfaceExporter: InterfaceExporter {
         _ parameter: EndpointParameter<Type>,
         for request: SomeInput
     ) throws -> Type?? where Type: Decodable, Type: Encodable {
-        if let inputParameter = request.parameters[parameter.name] as? WebSocketInfrastructure.NullableOptionalVariableParameter<Type> {
+        if let inputParameter = request.parameters[parameter.name] as? WebSocketInfrastructure.BasicInputParameter<Type> {
             return inputParameter.value
         } else {
             return nil
@@ -85,6 +112,51 @@ class WebSocketInterfaceExporter: InterfaceExporter {
     }
     
     func exportParameter<Type>(_ parameter: EndpointParameter<Type>) -> (String, InputParameter) where Type: Decodable, Type: Encodable {
-        (parameter.name, WebSocketInfrastructure.NullableOptionalVariableParameter<Type>())
+        (parameter.name, WebSocketInfrastructure.BasicInputParameter<Type>())
+    }
+    
+    private static func handleInputCompletion(
+        with latestInput: SomeInput,
+        using context: inout AnyConnectionContext<WebSocketInterfaceExporter>,
+        on eventLoop: EventLoop,
+        output: PassthroughSubject<Message<AnyEncodable>, Error>) {
+        context.handle(request: latestInput, eventLoop: eventLoop, final: true).whenComplete { result in
+            switch result {
+            case .success(.nothing):
+                output.send(completion: .finished)
+            case .success(.send(let message)):
+                output.send(.message(message))
+                output.send(completion: .finished)
+            case .success(.final(let message)):
+                output.send(.message(message))
+                output.send(completion: .finished)
+            case .success(.end):
+                output.send(completion: .finished)
+            case .failure(let error):
+                output.send(completion: .failure(error))
+            }
+        }
+    }
+    
+    private static func handleRegularInput(
+        with latestInput: SomeInput,
+        using context: inout AnyConnectionContext<WebSocketInterfaceExporter>,
+        on eventLoop: EventLoop,
+        output: PassthroughSubject<Message<AnyEncodable>, Error>) {
+        context.handle(request: latestInput, eventLoop: eventLoop, final: false).whenComplete { result in
+            switch result {
+            case .success(.nothing):
+                break
+            case .success(.send(let message)):
+                output.send(.message(message))
+            case .success(.final(let message)):
+                output.send(.message(message))
+                output.send(completion: .finished)
+            case .success(.end):
+                output.send(completion: .finished)
+            case .failure(let error):
+                output.send(.error(error))
+            }
+        }
     }
 }
