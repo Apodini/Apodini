3919b454b108f4e00d0b5c38a6c6fe9267dc9776
Tim Gymnich <tgymnich@icloud.com>
2021-01-13 00:34:25 +0100

    stand alone apns client

--- Sources/Apodini/Configurations/Application/Application+APNS.swift
+++ Sources/Apodini/Configurations/Application/Application+APNS.swift
@@ -6,6 +6,10 @@
 //
 
 import APNS
+import Logging
+import NIO
+import Foundation
+@_implementationOnly import AsyncKit
 
 
 extension Application {
@@ -31,6 +35,84 @@ extension Application {
             }
         }
 
+        struct PoolKey: StorageKey, LockKey {
+            typealias Value = EventLoopGroupConnectionPool<APNSConnectionSource>
+        }
+
+        internal var pool: EventLoopGroupConnectionPool<APNSConnectionSource> {
+            if let existing = self.application.storage[PoolKey.self] {
+                return existing
+            } else {
+                let lock = self.application.locks.lock(for: PoolKey.self)
+                lock.lock()
+                defer { lock.unlock() }
+                guard let configuration = self.configuration else {
+                    fatalError("APNS not configured. Use app.apns.configuration = ...")
+                }
+                let new = EventLoopGroupConnectionPool(
+                    source: APNSConnectionSource(configuration: configuration),
+                    maxConnectionsPerEventLoop: 1,
+                    logger: self.application.logger,
+                    on: self.application.eventLoopGroup
+                )
+                self.application.storage.set(PoolKey.self, to: new) {
+                    $0.shutdown()
+                }
+                return new
+            }
+        }
+
         let application: Application
+    }
+}
+
+extension Application.APNS: APNSwiftClient {
+    public var logger: Logger? {
+        self.application.logger
+    }
+
+    public var eventLoop: EventLoop {
+        self.application.eventLoopGroup.next()
+    }
+
+    public func send(
+        rawBytes payload: ByteBuffer,
+        pushType: APNSwiftConnection.PushType,
+        to deviceToken: String,
+        expiration: Date?,
+        priority: Int?,
+        collapseIdentifier: String?,
+        topic: String?,
+        logger: Logger?
+    ) -> EventLoopFuture<Void> {
+        self.application.apns.pool.withConnection(
+            logger: logger,
+            on: self.eventLoop
+        ) {
+            $0.send(
+                rawBytes: payload,
+                pushType: pushType,
+                to: deviceToken,
+                expiration: expiration,
+                priority: priority,
+                collapseIdentifier: collapseIdentifier,
+                topic: topic,
+                logger: logger
+            )
+        }
+    }
+}
+
+internal final class APNSConnectionSource: ConnectionPoolSource {
+    private let configuration: APNSwiftConfiguration
+
+    public init(configuration: APNSwiftConfiguration) {
+        self.configuration = configuration
+    }
+    public func makeConnection(
+        logger: Logger,
+        on eventLoop: EventLoop
+    ) -> EventLoopFuture<APNSwiftConnection> {
+        APNSwiftConnection.connect(configuration: self.configuration, on: eventLoop)
     }
 }
