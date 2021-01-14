b46e8c4c0d9e56657dda6aaee20adfaa628826de
Nityananda Zbil <nityananda.zbil@icloud.com>
2021-01-14 17:15:39 +0100

    Merge ProtobufferBuilderTests

--- Tests/ApodiniTests/ProtobufferBuilderTests.swift
+++ Tests/ApodiniTests/ProtobufferBuilderTests.swift
@@ -1,11 +1,31 @@
-import XCTest
+import XCTVapor
 @testable import Apodini
 
-final class ProtobufferBuilderInternalTests: XCTestCase {}
+final class ProtobufferBuilderTests: XCTestCase {}
+
+private extension ProtobufferBuilderTests {
+    func testWebService<S: WebService>(_ type: S.Type, expectation: String) throws {
+        let app = Application(.testing)
+        S.main(app: app)
+        defer { app.shutdown() }
+        
+        try app.test(.GET, "apodini/proto") { res in
+            XCTAssertEqual(res.body.string, expectation)
+        }
+    }
+    
+    func buildMessage<T>(_ type: T.Type) throws -> String {
+        let builder = ProtobufferBuilder()
+        try builder.addMessage(messageType: type)
+        
+        return builder.description
+    }
+
+}
 
 // MARK: - Test Supported Types
 
-extension ProtobufferBuilderInternalTests {
+extension ProtobufferBuilderTests {
     func testVoid() throws {
         XCTAssertNoThrow(try buildMessage(Void.self))
     }
@@ -43,7 +63,7 @@ extension ProtobufferBuilderInternalTests {
 
 // MARK: - Test Messages
 
-extension ProtobufferBuilderInternalTests {
+extension ProtobufferBuilderTests {
     func testScalarType() throws {
         let expected = """
             syntax = "proto3";
@@ -199,19 +219,114 @@ extension ProtobufferBuilderInternalTests {
     }
 }
 
-// MARK: - Private
+// MARK: - Test Components
 
-@discardableResult
-private func buildMessage<T>(_ type: T.Type) throws -> String {
-    let builder = ProtobufferBuilder()
-    try builder.addMessage(messageType: type)
-    let description = builder.description
+extension ProtobufferBuilderTests {
+    func testServiceParametersZero() throws {
+        struct WebService: Apodini.WebService {
+            var content: some Component {
+                HelloWorld()
+            }
+        }
+        
+        struct HelloWorld: Handler {
+            func handle() -> String {
+                "Hello, World!"
+            }
+        }
+        
+        let expected = """
+            syntax = "proto3";
+
+            service V1Service {
+              rpc helloworld (VoidMessage) returns (StringMessage);
+            }
+
+            message StringMessage {
+              string value = 1;
+            }
+
+            message VoidMessage {}
+            """
+        
+        try testWebService(WebService.self, expectation: expected)
+    }
     
-    print("""
-        ----
-        \(description)
-        ----
-        """)
+    func testServiceParametersOne() throws {
+        struct WebService: Apodini.WebService {
+            var content: some Component {
+                Greeter()
+            }
+        }
+        
+        struct Greeter: Handler {
+            @Parameter
+            var name: String
+            
+            func handle() -> String {
+                "Hello \(name)"
+            }
+        }
+        
+        let expected = """
+            syntax = "proto3";
+
+            service V1Service {
+              rpc greeter (StringMessage) returns (StringMessage);
+            }
+
+            message StringMessage {
+              string value = 1;
+            }
+            """
+        
+        try testWebService(WebService.self, expectation: expected)
+    }
     
-    return description
+    func testServiceParametersTwo() throws {
+        struct WebService: Apodini.WebService {
+            var content: some Component {
+                Multiplier()
+            }
+        }
+        
+        struct Multiplier: Handler {
+            @Parameter
+            var x: Int32
+            
+            @Parameter
+            var y: Int32
+            
+            func handle() -> Int32 {
+                x * y
+            }
+        }
+        
+        let expected = """
+            syntax = "proto3";
+
+            service V1Service {
+              rpc multiplier (MultiplierMessage) returns (Int32Message);
+            }
+
+            message Int32Message {
+              int32 value = 1;
+            }
+
+            message MultiplierMessage {
+              int32 x = 1;
+              int32 y = 2;
+            }
+            """
+        
+        try testWebService(WebService.self, expectation: expected)
+    }
+}
+
+// MARK: - Test Misc
+
+extension ProtobufferBuilderTests {
+    func testGenericPolymorphism() {
+        XCTAssertFalse(Array<Any>.self == Array<Int>.self)
+    }
 }
