b9283ac6793878bddec9be9c92a5b3ab2832616d
Lorena Schlesinger <lorena.schlesinger@tum.de>
2021-01-15 23:14:07 +0100

    Improve regression test for EnrichedInfo

--- Tests/ApodiniTests/SemanticModel/EnrichedInfoTests.swift
+++ Tests/ApodiniTests/SemanticModel/EnrichedInfoTests.swift
@@ -46,17 +46,21 @@ class EnrichedInfoTests: ApodiniTests {
 
     func testEnrichedInfoIsEquatable() throws {
         let stringType = EnrichedInfo(typeInfo: try typeInfo(of: String.self), propertyInfo: nil, propertiesOffset: nil)
-        let stringType2 = EnrichedInfo(typeInfo: try typeInfo(of: String.self), propertyInfo: nil, propertiesOffset: nil)
+        let stringType1 = EnrichedInfo(typeInfo: try typeInfo(of: String.self), propertyInfo: nil, propertiesOffset: nil)
         let intType = EnrichedInfo(typeInfo: try typeInfo(of: Int.self), propertyInfo: nil, propertiesOffset: nil)
         let complexReflectedType = try typeInfo(of: Array<Int>.self)
         let complexReflectedTypeProperty = try typeInfo(of: complexReflectedType.properties[0].type)
-        let complexTypePropertyInfo = EnrichedInfo(typeInfo: complexReflectedTypeProperty, propertyInfo: complexReflectedType.properties[0], propertiesOffset: 0)
-        let complexTypePropertyInfo2 = EnrichedInfo(typeInfo: complexReflectedTypeProperty, propertyInfo: complexReflectedType.properties[0], propertiesOffset: 1)
+        let complexTypePropertyInfo = EnrichedInfo(typeInfo: complexReflectedTypeProperty, propertyInfo: complexReflectedType.properties[0], propertiesOffset: 0, cardinality: .exactlyOne)
+        let complexTypePropertyInfo1 = EnrichedInfo(typeInfo: complexReflectedTypeProperty, propertyInfo: nil, propertiesOffset: 0, cardinality: .exactlyOne)
+        let complexTypePropertyInfo2 = EnrichedInfo(typeInfo: complexReflectedTypeProperty, propertyInfo: complexReflectedType.properties[0], propertiesOffset: 1, cardinality: .exactlyOne)
+        let complexTypePropertyInfo3 = EnrichedInfo(typeInfo: complexReflectedTypeProperty, propertyInfo: complexReflectedType.properties[0], propertiesOffset: 0, cardinality: .zeroToOne)
         
-        XCTAssertEqual(stringType, stringType2)
+        XCTAssertEqual(stringType, stringType1)
         XCTAssertNotEqual(stringType, intType)
         XCTAssertNotEqual(complexTypePropertyInfo, stringType)
         XCTAssertEqual(complexTypePropertyInfo, complexTypePropertyInfo)
+        XCTAssertNotEqual(complexTypePropertyInfo, complexTypePropertyInfo1)
         XCTAssertNotEqual(complexTypePropertyInfo, complexTypePropertyInfo2)
+        XCTAssertNotEqual(complexTypePropertyInfo, complexTypePropertyInfo3)
     }
 }
