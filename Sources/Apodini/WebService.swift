9b5028b1a3051bae41b037b48c924b88b55b0711
Paul Schmiedmayer <paul.schmiedmayer@tum.de>
2021-01-16 21:41:58 +0100

    Trigger SwiftLint

--- Sources/Apodini/WebService.swift
+++ Sources/Apodini/WebService.swift
@@ -10,6 +10,7 @@ import Logging
 
 /// Each Apodini program consists of a `WebService`component that is used to describe the Web API of the Web Service
 public protocol WebService: Component, ConfigurationCollection {
+    // TODO: Trigger SwitLint!
     /// The current version of the `WebService`
     var version: Version { get }
     
