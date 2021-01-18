3652d5480c922f46fed1be1a329d07dac9cb1e36
Lorena Schlesinger <lorena.schlesinger@tum.de>
2021-01-18 21:13:46 +0100

    add tests for isEnum, fixed code duplication from merge conflict

--- Sources/Apodini/Semantic Model Builder/Model/TypeReflection/EnrichedInfo.swift
+++ Sources/Apodini/Semantic Model Builder/Model/TypeReflection/EnrichedInfo.swift
@@ -23,50 +23,6 @@ struct EnrichedInfo {
     var cardinality: Cardinality = .exactlyOne
 }
 
-extension EnrichedInfo.Cardinality: Equatable {
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        switch (lhs, rhs) {
-        case (.zeroToOne, .zeroToOne), (.exactlyOne, .exactlyOne):
-            return true
-        case let (.zeroToMany(lhsCollection), .zeroToMany(rhsCollection)):
-            return (lhsCollection) == (rhsCollection)
-        default:
-            return false
-        }
-    }
-}
-
-extension EnrichedInfo.CollectionContext: Equatable {
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        switch (lhs, rhs) {
-        case (.array, .array):
-            return true
-        case let (.dictionary(lhsKey, lhsValue), .dictionary(rhsKey, rhsValue)):
-            return (lhsKey, lhsValue) == (rhsKey, rhsValue)
-        default:
-            return false
-        }
-    }
-}
-
-extension EnrichedInfo: Equatable {
-    public static func == (lhs: EnrichedInfo, rhs: EnrichedInfo) -> Bool {
-        if lhs.typeInfo.mangledName != rhs.typeInfo.mangledName {
-            return false
-        }
-        if lhs.propertyInfo?.name != rhs.propertyInfo?.name {
-            return false
-        }
-        if lhs.propertiesOffset != rhs.propertiesOffset {
-            return false
-        }
-        if lhs.cardinality != rhs.cardinality {
-            return false
-        }
-        return true
-    }
-}
-
 extension EnrichedInfo {
     static func node(_ type: Any.Type) throws -> Node<EnrichedInfo> {
         let typeInfo = try Runtime.typeInfo(of: type)
@@ -93,7 +49,6 @@ extension EnrichedInfo {
                         guard !errorDescription.contains(keyword) else {
                             return nil
                         }
-
                         preconditionFailure(errorDescription)
                     }
                 }
