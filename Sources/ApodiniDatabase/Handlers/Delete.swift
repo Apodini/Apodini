f1514c4273ce329bd97b3c5b0d09c5204b050b63
hendesi <felix@coolfly.de>
2021-01-19 12:35:59 +0100

    Proposal: Database Read Component using QueryBuilder  (#70)
    * First experiment with database components
    
    * moved query builder to separate file
    
    * generalize mechanism used by RequestInjectable and extend it to DynamicProperty and Dynamics
    
    * remove unwanted change
    
    * fix swiftlint
    
    * fix swiftlint 2
    
    * fix swiftlint 3
    
    * add assertion checking Dynamics is a struct
    
    * add tests
    
    * make Dynamics conform to ExpressibleByDictionaryLiteral
    
    * fix swiftlint again
    
    * fix ordering bug in execute test case
    
    * add @dynamicMemberLookup to Dynamics + typed<T>() function to retrieve typed array
    
    * make Dynamics generic (without restriction) + Dynamics conform to Collection
    
    * prepare for presentation
    
    * make Dynamics a property wrapper
    
    * hide Runtime + fix type + fix SwiftLint
    
    * replace usage of Mirror by execute
    
    * fix usage of executable
    
    * First draft of Get using Dynamics
    
    * First draft of a working query builder using Dynamics
    
    * clean up
    
    * Removed debug code and code relicts
    
    * Removed Param, create database property option to use Parameter
    
    * moved database components into seperate target
    
    * Renamed to ApodiniDatabase
    
    * Added descriptive comments
    
    * Added database handler tests
    
    * Fix swiftlint
    
    * fix swiftlint
    
    * Fix swiftlint
    
    * fix swiftlint
    
    * fix swiftlint
    
    * Fix update handler test
    
    * Fix build
    
    * fix test to pass on remote
    
    * removed useless parameter mode
    
    * Added testcase for querybuilder
    
    * Implemented feedback
    
    * Fix swiftlint
    
    * Fix swiftlint
    
    * First draft of type inference
    
    * Added support for partial update handler
    
    * Introduced visitor pattern to infer types
    
    * Introduced visitor pattern to infer types
    
    * Converted database handlers back to using eventloopfutures
    
    * Cleanup
    
    * removed unused class and added tests
    
    * Reworked database folder structure
    
    * Fix swiftlint
    
    * fix swiftlint + added tests for updater
    
    * fix swiftlint
    
    * fix swiftlint
    
    * swiftlint fix
    
    * fix build
    
    * Implemented feedback
    
    * Added comment on init + typo
    
    * added testcase for typecontainer
    
    * Refined testcase for type container
    
    * Implemented feedback by paul
    
    * Final clean up
    
    * fix sonarcloud warnings
    
    Co-authored-by: Felix Desiderato <felix.desiderato@tum.de>
    Co-authored-by: Max Obermeier <themomax@icloud.com>
    Co-authored-by: Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>

---
+++ Sources/ApodiniDatabase/Handlers/Delete.swift
@@ -0,0 +1,20 @@
+import Fluent
+import Apodini
+@_implementationOnly import Vapor
+
+/// A `Handler` that deletes the object with the given `IDValue` in the database, if it exists. If not an error is thrown.
+/// It uses the database that has been specified in the `DatabaseConfiguration`.
+public struct Delete<Model: DatabaseModel>: Handler {
+    @Apodini.Environment(\.database)
+    private var database: Fluent.Database
+
+    @Parameter(.http(.path))
+    var id: Model.IDValue
+    
+    public func handle() -> EventLoopFuture<UInt> {
+        Model.find(id, on: database)
+            .unwrap(orError: Abort(.notFound) )
+            .flatMap { $0.delete(on: database ) }
+            .transform(to: HTTPStatus.ok.code )
+    }
+}
