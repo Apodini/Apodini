86d52cdb2c0c84770fd87590a1da0abb251a445e
Tim Gymnich <tgymnich@icloud.com>
2021-02-02 19:28:30 +0100

    fix TestWebService

--- TestWebService/Package.swift
+++ TestWebService/Package.swift
@@ -18,7 +18,14 @@ let package = Package(
         .target(
             name: "TestWebService",
             dependencies: [
-                .product(name: "Apodini", package: "Apodini")
+                .product(name: "Apodini", package: "Apodini"),
+                .product(name: "ApodiniVaporSupport", package: "Apodini"),
+                .product(name: "ApodiniREST", package: "Apodini"),
+                .product(name: "ApodiniGRPC", package: "Apodini"),
+                .product(name: "ApodiniProtobuffer", package: "Apodini"),
+                .product(name: "ApodiniOpenAPI", package: "Apodini"),
+                .product(name: "ApodiniWebSocket", package: "Apodini"),
+                .product(name: "ApodiniNotifications", package: "Apodini")
             ]
         ),
         .testTarget(
