b7c9c2666f086d9193d690843cc2b07c457d3085
Sadık Ekin Özbay <ekin.oezbay@tum.de>
2021-02-02 14:49:06 +0100

    APNS update

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.1.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.1"),
         // Used by the `NotificationCenter` to send push notifications to `APNS`.
-        .package(url: "https://github.com/vapor/apns.git", from: "1.0.1"),
+        .package(url: "https://github.com/vapor/apns.git", from: "2.0.0"),
         // Used by the `NotificationCenter` to send push notifications to `FCM`.
         .package(url: "https://github.com/MihaelIsaev/FCM.git", from: "2.8.0"),
         .package(url: "https://github.com/vapor/fluent-mongo-driver.git", from: "1.0.2"),
