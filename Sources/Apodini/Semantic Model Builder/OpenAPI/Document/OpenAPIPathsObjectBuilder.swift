bc55d366d4d2a6c059b83fcd787721f7fcf0e224
Lorena Schlesinger <lorena.schlesinger@tum.de>
2021-02-05 19:53:25 +0100

    change tag default to FIRST string path component instead of last

--- Sources/Apodini/Semantic Model Builder/OpenAPI/Document/OpenAPIPathsObjectBuilder.swift
+++ Sources/Apodini/Semantic Model Builder/OpenAPI/Document/OpenAPIPathsObjectBuilder.swift
@@ -52,8 +52,8 @@ struct OpenAPIPathsObjectBuilder {
 private extension OpenAPIPathsObjectBuilder {
     /// https://swagger.io/specification/#operation-object
     mutating func buildPathItemOperationObject<H: Handler>(from endpoint: Endpoint<H>) -> OpenAPI.Operation {
-        // Get the string component appended last to the path as default tag, if no string component present, use "default" instead.
-        let defaultTag: String = endpoint.absolutePath.last { $0.isString() }?.description ?? "default"
+        // Get the string component appended first to the path as default tag, if no string component present, use "default" instead.
+        let defaultTag: String = endpoint.absolutePath.first { $0.isString() }?.description ?? "default"
         
         // Get tags if some have been set explicitly passed via TagModifier.
         let tags: [String] = endpoint.context.get(valueFor: TagContextKey.self) ?? [defaultTag]
