45232d97dbd290dbc8a25f496b9acf9e31620473
Lukas Kollmer <lukas.kollmer@gmail.com>
2021-03-01 11:16:32 +0100

    improve Conditions API
    - add support for comparison conditions (&& and ||)
    - capture a comparison's rhs as a closure, to make sure the expression is only evaluated if the condition itself it evaluated

--- Sources/ApodiniDeploy/DeploymentOptions/Condition.swift
+++ Sources/ApodiniDeploy/DeploymentOptions/Condition.swift
@@ -10,7 +10,7 @@ import Apodini
 import ApodiniDeployBuildSupport
 
 
-public struct AnyHandlerCondition {
+public class AnyHandlerCondition {
     let predicate: (Any) -> Bool
     
     init<H: Handler>(_: H.Type = H.self, _ predicate: @escaping (H) -> Bool) {
@@ -23,6 +23,25 @@ public struct AnyHandlerCondition {
 }
 
 
+public class HandlerCondition<H: Handler>: AnyHandlerCondition {
+    init(_: H.Type = H.self, _ predicate: @escaping (H) -> Bool) {
+        super.init(H.self, predicate)
+    }
+}
+
+public func && <H> (lhs: HandlerCondition<H>, rhs: HandlerCondition<H>) -> HandlerCondition<H> {
+    HandlerCondition { lhs.test(on: $0) && rhs.test(on: $0) }
+}
+
+public func || <H> (lhs: HandlerCondition<H>, rhs: HandlerCondition<H>) -> HandlerCondition<H> {
+    HandlerCondition { lhs.test(on: $0) || rhs.test(on: $0) }
+}
+
+public prefix func ! <H> (rhs: HandlerCondition<H>) -> HandlerCondition<H> {
+    return HandlerCondition { !rhs.test(on: $0) }
+}
+
+
 extension AnyOption {
     /// Limit the option to only take effect if the specified condition is satisfied
     public func when(_ condition: AnyHandlerCondition) -> AnyOption {
@@ -31,27 +50,27 @@ extension AnyOption {
 }
 
 
-public func == <H: Handler, P: Equatable> (lhs: KeyPath<H, P>, rhs: P) -> AnyHandlerCondition {
-    AnyHandlerCondition(H.self) { $0[keyPath: lhs] == rhs }
+public func == <H: Handler, P: Equatable> (lhs: KeyPath<H, P>, rhs: @autoclosure @escaping () -> P) -> HandlerCondition<H> {
+    HandlerCondition { $0[keyPath: lhs] == rhs() }
 }
 
-public func != <H: Handler, P: Equatable> (lhs: KeyPath<H, P>, rhs: P) -> AnyHandlerCondition {
-    AnyHandlerCondition(H.self) { $0[keyPath: lhs] != rhs }
+public func != <H: Handler, P: Equatable> (lhs: KeyPath<H, P>, rhs: @autoclosure @escaping () -> P) -> HandlerCondition<H> {
+    HandlerCondition { $0[keyPath: lhs] != rhs() }
 }
 
 
-public func < <H: Handler, P: Comparable> (lhs: KeyPath<H, P>, rhs: P) -> AnyHandlerCondition {
-    AnyHandlerCondition(H.self) { $0[keyPath: lhs] < rhs }
+public func < <H: Handler, P: Comparable> (lhs: KeyPath<H, P>, rhs: @autoclosure @escaping () -> P) -> HandlerCondition<H> {
+    HandlerCondition { $0[keyPath: lhs] < rhs() }
 }
 
-public func > <H: Handler, P: Comparable> (lhs: KeyPath<H, P>, rhs: P) -> AnyHandlerCondition {
-    AnyHandlerCondition(H.self) { $0[keyPath: lhs] > rhs }
+public func > <H: Handler, P: Comparable> (lhs: KeyPath<H, P>, rhs: @autoclosure @escaping () -> P) -> HandlerCondition<H> {
+    HandlerCondition { $0[keyPath: lhs] > rhs() }
 }
 
-public func <= <H: Handler, P: Comparable> (lhs: KeyPath<H, P>, rhs: P) -> AnyHandlerCondition {
-    AnyHandlerCondition(H.self) { $0[keyPath: lhs] <= rhs }
+public func <= <H: Handler, P: Comparable> (lhs: KeyPath<H, P>, rhs: @autoclosure @escaping () -> P) -> HandlerCondition<H> {
+    HandlerCondition { $0[keyPath: lhs] <= rhs() }
 }
 
-public func >= <H: Handler, P: Comparable> (lhs: KeyPath<H, P>, rhs: P) -> AnyHandlerCondition {
-    AnyHandlerCondition(H.self) { $0[keyPath: lhs] <= rhs }
+public func >= <H: Handler, P: Comparable> (lhs: KeyPath<H, P>, rhs: @autoclosure @escaping () -> P) -> HandlerCondition<H> {
+    HandlerCondition { $0[keyPath: lhs] <= rhs() }
 }
