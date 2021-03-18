4d5b4fecaeaa5a819895412af643cacc141e6ec1
Lukas Kollmer <lukas.kollmer@gmail.com>
2021-03-18 11:00:26 +0100

    update TestWebService required macOS version

--- TestWebService/Package.swift
+++ TestWebService/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TestWebService",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v11)
     ],
     products: [
         .executable(name: "TestWebService", targets: ["TestWebService"])
