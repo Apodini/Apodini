4915cd04c6f6a1fcc3b7cfb4e20e927248d886ae
Lukas Kollmer <lukas.kollmer@gmail.com>
2021-03-23 22:19:55 +0100

    Fix issue w/ wrong overload resolution in http configuration command line arguments parser

--- Sources/ApodiniUtils/String.swift
+++ Sources/ApodiniUtils/String.swift
@@ -16,19 +16,28 @@ extension String {
         return self + suffix
     }
     
+    
     /// Creates a string by interpreting a tuple of `UInt8` values as a C string.
     /// This is useful when working with C libraries, where the swift importer sometimes represents C-style arrays as tuples.
-    public init?(int8Tuple: Any) {
+    /// - Note: This intentionally is a static function rather than an initializer, the reason being that,
+    ///         due to the fact that this function takes `Any`, it would cause the typechecker to, in some cases,  prefer this `init` overload
+    ///         over other better-matching overloads when initialising a string from some (generic) value.
+    ///         Note that this even applies if the initializer has a named argument: if its called `init?(int8Tuple:)` and you write code like
+    ///         `[Substring]().first.flatMap(String.init)`, the typechecker will resolve the `String.init` to the tuple-taking overload,
+    ///         rather than the probably intended `String.init(Substring)` overload.
+    public static func createFromInt8Tuple(_ int8Tuple: Any) -> String? {
         let mirror = Mirror(reflecting: int8Tuple)
         guard mirror.displayStyle == .tuple else {
             return nil
         }
-        self.init()
+        var str = ""
+        str.reserveCapacity(mirror.children.count)
         for (_, value) in mirror.children {
             guard let value = value as? Int8 else {
-                return
+                return nil
             }
-            append(String(UnicodeScalar(UInt8(value))))
+            str.append(String(UnicodeScalar(UInt8(value))))
         }
+        return str
     }
 }
