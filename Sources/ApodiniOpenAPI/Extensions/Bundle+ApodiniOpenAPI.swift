cb3c0df026c9198b044f9c45de75ea1903472626
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2021-05-17 21:56:29 +0200

    Update Dependencies & Use Resources for Unit Tests (#279)
    * Update dependencies
    
    * Fix test due to updates in NIOSSL (NIOSSLPrivateKeySource)
    Use resources for unit tests instead of absolute paths
    
    * Update dependencies for the TestWebService
    
    * Fix SwiftLint warning

---
+++ Sources/ApodiniOpenAPI/Extensions/Bundle+ApodiniOpenAPI.swift
@@ -0,0 +1,16 @@
+//
+//  Bundle+ApodiniOpenAPI.swift
+//  
+//
+//  Created by Paul Schmiedmayer on 5/17/21.
+//
+
+import Foundation
+
+
+extension Bundle {
+    /// Resources associated with the ApodiniOpenAPI target
+    public static var apodiniOpenAPIResources: Bundle {
+        .module
+    }
+}
