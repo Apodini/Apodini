dd9a804ed516d4325c1b3ae9681576bc57b2e428
Philipp Zagar <philipp.zagar@gmail.com>
2021-06-21 17:27:33 +0200

    Silence linter (hopefully the last time)

--- Sources/ApodiniGRPC/GRPCDependentStaticConfiguration.swift
+++ Sources/ApodiniGRPC/GRPCDependentStaticConfiguration.swift
@@ -9,7 +9,6 @@ import Apodini
 
 /// `GRPCDependentStaticConfiguration`s are used to register static services dependend on the `GRPCInterfaceExporter`
 public protocol GRPCDependentStaticConfiguration {
-    
     /// A method that handels the configuration of dependend static exporters
     /// - Parameters:
     ///    - app: The `Vapor.Application` which is used to register the configuration in Apodini
