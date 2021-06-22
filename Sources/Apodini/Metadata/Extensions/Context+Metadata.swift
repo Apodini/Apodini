98a9a1b1787ca20cb2a4832cb63658065034d901
Andreas Bauer <andi.bauer@tum.de>
2021-06-22 11:38:07 +0200

    Silence compiler warning about duplicate conformance to OptionalContextKey

--- Sources/Apodini/Metadata/Extensions/Context+Metadata.swift
+++ Sources/Apodini/Metadata/Extensions/Context+Metadata.swift
@@ -7,8 +7,7 @@ public extension Context {
     /// Retrieves the value for a given `MetadataDefinition`.
     /// - Parameter contextKey: The `MetadataDefinition` to retrieve the value for.
     /// - Returns: Returns the stored value or `nil` if it does not exist on the given `Context`.
-    func get<Metadata: MetadataDefinition>(valueFor metadata: Metadata.Type = Metadata.self) -> Metadata.Key.Value?
-        where Metadata.Key: OptionalContextKey {
+    func get<Metadata: MetadataDefinition>(valueFor metadata: Metadata.Type = Metadata.self) -> Metadata.Key.Value? {
         get(valueFor: metadata.Key.self)
     }
 
