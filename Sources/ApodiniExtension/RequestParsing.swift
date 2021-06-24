b21667c757ee1ddc2f79fe459f32d1b3d67134d1
Max Obermeier <themomax@icloud.com>
2021-06-24 17:16:35 +0200

    implement EndpointParameter interoperability for ParsingStrategy

--- Sources/ApodiniExtension/RequestParsing.swift
+++ Sources/ApodiniExtension/RequestParsing.swift
@@ -14,9 +14,63 @@ public protocol ParsingStrategy {
     func strategy<Element: Decodable>(for parameter: Parameter<Element>) -> AnyParameterParsingStrategy<Element>
 }
 
+public protocol EndpointParameterParsingStrategy {
+    func strategy<Element: Decodable>(for parameter: EndpointParameter<Element>) -> AnyParameterParsingStrategy<Element>
+}
 
+public struct EndpointParameterBasedParsingStrategy<S: EndpointParameterParsingStrategy>: ParsingStrategy {
+    private let strategy: S
+    
+    private let endpointParameters: [UUID: AnyEndpointParameter]
+    
+    public init(_ strategy: S, on endpoint: AnyEndpoint) {
+        self.strategy = strategy
+        self.endpointParameters = endpoint[EndpointParameters.self].reduce(into: [UUID: AnyEndpointParameter](), { storage, parameter in
+            storage[parameter.id] = parameter
+        })
+    }
+    
+    public func strategy<Element: Decodable>(for parameter: Parameter<Element>) -> AnyParameterParsingStrategy<Element> {
+        guard let parameter = endpointParameters[parameter.id] as? CanCallEndpointParameterParsingStrategy else {
+            fatalError("Couldn't find matching 'EndpointParameter' with id \(parameter.id) while determining 'ParsingStrategy'.")
+        }
+        
+        return parameter.call(strategy)
+    }
+}
 
+private protocol CanCallEndpointParameterParsingStrategy {
+    func call<S: EndpointParameterParsingStrategy, Element: Decodable>(_ strategy: S) -> AnyParameterParsingStrategy<Element>
+}
 
+extension EndpointParameter: CanCallEndpointParameterParsingStrategy {
+    func call<S, V>(_ strategy: S) -> AnyParameterParsingStrategy<V> where S : EndpointParameterParsingStrategy, V : Decodable {
+        let baseStrategy = strategy.strategy(for: self)
+        if nilIsValidValue { // V == Optional<Type>
+            if let typedStrategy = OptionalWrappingStrategy(baseStrategy: baseStrategy).typeErased as? AnyParameterParsingStrategy<V> {
+                return typedStrategy
+            }
+            fatalError("Internal logic of 'EndpointParameter.call(_:)' is broken: wrong type in nil case.")
+        } else { // V == Type
+            if let typedStrategy = baseStrategy as? AnyParameterParsingStrategy<V> {
+                return typedStrategy
+            }
+            fatalError("Internal logic of 'EndpointParameter.call(_:)' is broken: wrong type in base case.")
+        }
+    }
+}
+
+private struct OptionalWrappingStrategy<P: ParameterParsingStrategy>: ParameterParsingStrategy {
+    let baseStrategy: P
+    
+    func decode(from decoder: Decoder) throws -> Optional<P.Element> {
+        do {
+            return .some(try baseStrategy.decode(from: decoder))
+        } catch DecodingError.valueNotFound(_, _) {
+            return .none
+        }
+    }
+}
 
 // MARK: ParameterParsingStrategy
 
