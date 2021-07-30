4a148c8950d1822fa2bee2f12e231a011d6a9b41
Andreas Bauer <andi.bauer@tum.de>
2021-07-30 17:17:10 +0200

    Ensure REUSE compliance

--- Tests/ApodiniAuthorizationTests/AuthorizationRequirementsTests.swift
+++ Tests/ApodiniAuthorizationTests/AuthorizationRequirementsTests.swift
@@ -1,5 +1,9 @@
 //
-// Created by Andreas Bauer on 15.07.21.
+// This source file is part of the Apodini open source project
+//
+// SPDX-FileCopyrightText: 2019-2021 Paul Schmiedmayer and the Apodini project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+//
+// SPDX-License-Identifier: MIT
 //
 
 import XCTApodini
