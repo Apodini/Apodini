2c0ff1e5e9c504dd191f00b87684fda331efe783
Andreas Bauer <andi.bauer@tum.de>
2021-08-16 23:50:45 +0200

    Reenable negative compile tests

--- Tests/NegativeCompileTestsRunner/configurations.swift
+++ Tests/NegativeCompileTestsRunner/configurations.swift
@@ -13,10 +13,7 @@ let configurations: TestRunnerConfiguration = [
     .target(
         name: "ApodiniNegativeCompileTests",
         configurations: [
-            // Linux has issues compiling malformed resultBuilders, resulting in one compiler error
-            // "failed to produce diagnostic for expression; please file a bug report" for the whole block
-            // when too many errors occurred inside. Therefore linux platform is excluded.
-            .testCase("Metadata", runningOn: .exclude(.linux))
+            .testCase("Metadata")
         ]
     )
 ]
