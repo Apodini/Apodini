dce6a16bf16bd9a43de2afbde70c5a2c5ee56e4c
Philipp, Zagar <philipp.zagar@gmail.com>
2021-08-19 14:23:05 +0200

    Fix Decoding problems by re-introducing the IntermediateRepresentation

--- Sources/ApodiniLoggingSupport/Logger.MetadataValue+Codable.swift
+++ Sources/ApodiniLoggingSupport/Logger.MetadataValue+Codable.swift
@@ -9,8 +9,8 @@
 import Foundation
 import Logging
 
-/// Make ``Logger.MetadataValue`` ``Codable``
-extension Logger.MetadataValue: Codable {
+/// Make ``Logger.MetadataValue`` ``Encodable``
+extension Logger.MetadataValue: Encodable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         
@@ -25,22 +25,59 @@ extension Logger.MetadataValue: Codable {
             try container.encode(array)
         }
     }
+}
+
+/// IntermediateRepresentation to convert any ``Encodable`` data type to ``Logger.MetadataValue``
+enum LoggerMetadataIntermediateRepresentation: Codable {
+    case null
+    case bool(Bool)
+    case int(Int)
+    case double(Double)
+    case string(String)
+    case array([LoggerMetadataIntermediateRepresentation])
+    case dictionary([String: LoggerMetadataIntermediateRepresentation])
     
-    public init(from decoder: Decoder) throws {
+    
+    var metadataValue: Logger.MetadataValue {
+        switch self {
+        case .null:
+            return .string("null")
+        case let .bool(bool):
+            return .string("\(bool)")
+        case let .int(int):
+            return .string("\(int)")
+        case let .double(double):
+            return .string("\(double)")
+        case let .string(string):
+            return .string(string)
+        case let .array(array):
+            return .array(array.map({ $0.metadataValue }))
+        case let .dictionary(dictionary):
+            return .dictionary(dictionary.mapValues({ $0.metadataValue }))
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        
+         
         if container.decodeNil() {
-            self = .string("null")
+            self = .null
+        } else if let bool = try? container.decode(Bool.self) {
+            self = .bool(bool)
+        } else if let int = try? container.decode(Int.self) {
+            self = .int(int)
+        } else if let double = try? container.decode(Double.self) {
+            self = .double(double)
         } else if let string = try? container.decode(String.self) {
             self = .string(string)
-        } else if let array = try? container.decode([Logger.MetadataValue].self) {
+        } else if let array = try? container.decode([LoggerMetadataIntermediateRepresentation].self) {
             self = .array(array)
-        } else if let dictionary = try? container.decode(Logger.Metadata.self) {
+        } else if let dictionary = try? container.decode([String: LoggerMetadataIntermediateRepresentation].self) {
             self = .dictionary(dictionary)
         } else {
-            throw DecodingError.dataCorrupted(
-                DecodingError.Context(codingPath: decoder.codingPath,
-                                      debugDescription: "Encountered unexpected JSON values")
+            throw DecodingError.dataCorrupted(DecodingError.Context(
+                codingPath: decoder.codingPath,
+                debugDescription: "Encountered unexpected JSON values")
             )
         }
     }
@@ -68,7 +105,7 @@ extension Logger.MetadataValue {
     
     /// Converts a ``Data`` element to ``Logger.MetadataValue``
     public static func convertToMetadata(data: Data) throws -> Logger.MetadataValue {
-        try Self.jsonDecoder.decode(Logger.MetadataValue.self, from: data)
+        try JSONDecoder().decode(LoggerMetadataIntermediateRepresentation.self, from: data).metadataValue
     }
     
     /// Converts ``Logger.MetadataValue`` to ``Data``
