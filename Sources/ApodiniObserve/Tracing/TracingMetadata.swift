b12ea9fa16bccde3509de55ada88d177ff1ab3b7
Moritz Sternemann <m.sternemann@tum.de>
2022-01-08 21:56:51 +0100

    Add metadata to enable tracing

---
+++ Sources/ApodiniObserve/Tracing/TracingMetadata.swift
@@ -0,0 +1,34 @@
+//
+// This source file is part of the Apodini open source project
+//
+// SPDX-FileCopyrightText: 2019-2021 Paul Schmiedmayer and the Apodini project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+//
+// SPDX-License-Identifier: MIT
+//
+
+import Apodini
+
+// TODO: Maybe non-optional with default of true
+public struct TracingContextKey: OptionalContextKey {
+    public typealias Value = Bool
+}
+
+extension ComponentMetadataNamespace {
+    public typealias Tracing = TracingMetadata
+}
+
+public struct TracingMetadata: ComponentMetadataDefinition {
+    public typealias Key = TracingContextKey
+
+    public var value: Bool
+
+    public init(isEnabled: Bool) {
+        self.value = isEnabled
+    }
+}
+
+extension Component {
+    public func trace(isEnabled: Bool = true) -> ComponentMetadataModifier<Self> {
+        self.metadata(TracingMetadata(isEnabled: true))
+    }
+}
