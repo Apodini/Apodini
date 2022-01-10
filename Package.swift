9e1b8fa4f173b151f972f7f89993b0acb14087ac
Moritz Sternemann <m.sternemann@tum.de>
2022-01-10 22:54:30 +0100

    Revert "Add exectuable to try out forwarding"
    This reverts commit b5d51eaefb2d4feb0bb39affb04524848bc74a4d.

--- Package.swift
+++ Package.swift
@@ -61,10 +61,7 @@ let package = Package(
         // Test Utils
         .library(name: "XCTApodini", targets: ["XCTApodini"]),
         .library(name: "XCTApodiniObserve", targets: ["XCTApodiniObserve"]),
-        .library(name: "XCTApodiniNetworking", targets: ["XCTApodiniNetworking"]),
-
-        // Moritz Test
-        .executable(name: "ErrorForwardingTest", targets: ["ErrorForwardingTest"])
+        .library(name: "XCTApodiniNetworking", targets: ["XCTApodiniNetworking"])
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.16.0"),
@@ -753,15 +750,6 @@ let package = Package(
                 .product(name: "SwiftLogTesting", package: "swift-log-testing"),
                 .product(name: "Metrics", package: "swift-metrics"),
                 .product(name: "MetricsTestUtils", package: "swift-metrics-extras")
-            ]
-        ),
-
-        // Moritz Test
-        .executableTarget(
-            name: "ErrorForwardingTest",
-            dependencies: [
-                .target(name: "Apodini"),
-                .target(name: "ApodiniHTTP")
             ]
         )
     ]
