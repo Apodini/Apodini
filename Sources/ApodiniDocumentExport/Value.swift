27b282f4839a569cd3f00b11b625484ea5e5ca6f
Valentin Bootz <valentin.bootz@icloud.com>
2022-01-27 11:50:51 +0100

    Revert "Add ApodiniDocumentExport"
    This reverts commit 39cb4c294f8fd2a664b7c19ddafa4390ed7a6316.

--- Sources/ApodiniDocumentExport/Value.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-/// A protocol that requires conformance to `Codable` and `Hashable` (also `Equatable`)
-public protocol Value: Codable, Hashable {}
-
-extension Array: Value where Element: Value {}
