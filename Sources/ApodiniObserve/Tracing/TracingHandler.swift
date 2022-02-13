6c5d6940c6a10474c5b8c231302816e510d3db29
Moritz Sternemann <m.sternemann@tum.de>
2022-02-13 13:11:28 +0100

    Record additional metadata for errors

--- Sources/ApodiniObserve/Tracing/TracingHandler.swift
+++ Sources/ApodiniObserve/Tracing/TracingHandler.swift
@@ -16,8 +16,12 @@ struct TracingHandler<H: Handler>: Handler {
     @Environment(\.connection)
     var connection
 
-    @ObserveMetadata
-    var observeMetadata // TODO: Maybe only get the blackboard metadata to reduce performance impact
+    @Environment(\ObserveMetadataExporter.BlackboardObserveMetadata.value)
+    var blackboardMetadata
+
+    /// Logging metadata is only accessed in case of a server error.
+    @LoggingMetadata
+    var loggingMetadata
 
     @Environment(\.instrument)
     var instrument
@@ -33,6 +37,8 @@ struct TracingHandler<H: Handler>: Handler {
         // Extract context from HTTP headers
         // e.g. OTel's W3CPropagator handles extracting W3C context propagation headers
         var baggage = Baggage.topLevel
+        // We need to use the instrument here because the tracer variable tracer is only the first available `Tracer` instance
+        // and we might want to extract baggage from other instruments as well.
         instrument.extract(connection.information, into: &baggage, using: HTTPInformationExtractor())
 
         let span = tracer.startSpan(getOperationName(), baggage: baggage, ofKind: .server)
@@ -48,32 +54,63 @@ struct TracingHandler<H: Handler>: Handler {
 
             // TODO: Can we get the response status here somehow
         } catch {
-            span.recordError(error)
+            setErrorMetadata(to: span, for: error)
             throw error // rethrow for InterfaceExporter handling
         }
     }
 
     private func getOperationName() -> String {
-        "\(observeMetadata.blackboardMetadata.operation.description) \(observeMetadata.blackboardMetadata.endpointName)"
+        "\(blackboardMetadata.operation.description) \(blackboardMetadata.endpointName)"
     }
 
     private func setEndpointMetadata(to span: Span) {
-        span.attributes.apodini.endpointName = observeMetadata.blackboardMetadata.endpointName
-        span.attributes.apodini.endpointOperation = observeMetadata.blackboardMetadata.operation.description
+        span.attributes.apodini.endpointName = blackboardMetadata.endpointName
+        span.attributes.apodini.endpointOperation = blackboardMetadata.operation.description
         span.attributes.apodini.endpointPath = String(
-            observeMetadata.blackboardMetadata.endpointPathComponents.value
+            blackboardMetadata.endpointPathComponents.value
                 .reduce(into: "") { path, endpointPath in
                     path.append(contentsOf: endpointPath.description + "/")
                 }
                 .dropLast()
         )
-        span.attributes.apodini.endpointCommunicationalPattern = observeMetadata.blackboardMetadata.communicationalPattern.rawValue
-        span.attributes.apodini.endpointVersion = observeMetadata.blackboardMetadata.context.get(valueFor: APIVersionContextKey.self)?.debugDescription ?? "unknown"
+        span.attributes.apodini.endpointCommunicationalPattern = blackboardMetadata.communicationalPattern.rawValue
+        span.attributes.apodini.endpointVersion = blackboardMetadata.context.get(valueFor: APIVersionContextKey.self)?.debugDescription ?? "unknown"
+    }
+
+    private func setErrorMetadata(to span: Span, for error: Error) {
+        // Tracers need to convert an Error to a String to record it. The OTel implementation uses String(describing:) internally.
+        // Because ApodiniError currently doesn't conform to CustomStringConvertible/CustomDebugStringConvertible,
+        // this results in a quite an ugly string representation in the trace.
+        span.recordError(error)
+        span.setStatus(.init(code: .error, message: error.standardMessage))
+
+        // We only use expensive loggingMetadata in case of a "server error"
+        guard error.apodiniError.shouldCollectErrorMetadata else { return }
+
+        for metadataKey in ["request", "information"] {
+            guard let metadataValue = loggingMetadata[metadataKey] else { continue }
+            span.attributes.merge(metadataValue.spanAttributes(key: "apodini.\(metadataKey)"))
+        }
+    }
     }
 }
 
 struct TracingHandlerInitializer: DelegatingHandlerInitializer {
     func instance<D: Handler>(for delegate: D) throws -> SomeHandler<Never> {
         SomeHandler(TracingHandler(delegate))
+    }
+}
+
+extension ApodiniError {
+    var shouldCollectErrorMetadata: Bool {
+        switch option(for: .errorType) {
+        case .serverError, .notAvailable, .other:
+            // These errors are caused by unexpected behavior of the service (correspond to HTTP 5xx)
+            // or are non-ApodiniErrors
+            return true
+        default:
+            // The remaining errors are caused by the client (correspond to HTTP 4xx)
+            return false
+        }
     }
 }
