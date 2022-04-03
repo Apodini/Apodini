fb094d91bd0cd34ff268d41ac8cff152120c80c6
Simon Bohnen <simon.bohnen@tum.de>
2022-04-03 16:29:36 -0700

    Change file headers and rename AuditReport.swift to Audit.swift

--- Sources/ApodiniAudit/Audit/Audit.swift
+++ Sources/ApodiniAudit/Audit/Audit.swift
@@ -1,8 +1,9 @@
 //
-//  File.swift
-//  
+// This source file is part of the Apodini open source project
 //
-//  Created by Simon Bohnen on 4/3/22.
+// SPDX-FileCopyrightText: 2019-2021 Paul Schmiedmayer and the Apodini project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+//
+// SPDX-License-Identifier: MIT
 //
 
 import Foundation
