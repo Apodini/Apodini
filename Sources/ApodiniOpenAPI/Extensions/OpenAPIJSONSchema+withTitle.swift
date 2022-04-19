ff4c2f27bc1cb56ec733f8eac6b3033de32f52c2
Simon Bohnen <simon.bohnen@tum.de>
2022-04-18 22:28:40 -0700

    More swiftlint

--- Sources/ApodiniOpenAPI/Extensions/OpenAPIJSONSchema+withTitle.swift
+++ Sources/ApodiniOpenAPI/Extensions/OpenAPIJSONSchema+withTitle.swift
@@ -11,30 +11,29 @@ import OpenAPIKit
 
 extension JSONSchema {
     // swiftlint:disable:next cyclomatic_complexity
-    // swiftlint:disable pattern_matching_keywords
     func with(title: String) -> JSONSchema {
         switch self {
-        case .boolean(let context):
+        case let .boolean(context):
             return .boolean(context.with(title: title))
-        case .object(let contextA, let contextB):
+        case let .object(contextA, contextB):
             return .object(contextA.with(title: title), contextB)
-        case .array(let contextA, let contextB):
+        case let .array(contextA, contextB):
             return .array(contextA.with(title: title), contextB)
-        case .number(let context, let contextB):
+        case let .number(context, contextB):
             return .number(context.with(title: title), contextB)
-        case .integer(let context, let contextB):
+        case let .integer(context, contextB):
             return .integer(context.with(title: title), contextB)
-        case .string(let context, let contextB):
+        case let .string(context, contextB):
             return .string(context.with(title: title), contextB)
-        case .fragment(let context):
+        case let .fragment(context):
             return .fragment(context.with(title: title))
-        case .all(of: let fragments, core: let core):
+        case let .all(of: fragments, core: core):
             return .all(of: fragments, core: core.with(title: title))
-        case .one(of: let schemas, core: let core):
+        case let .one(of: schemas, core: core):
             return .one(of: schemas, core: core.with(title: title))
-        case .any(of: let schemas, core: let core):
+        case let .any(of: schemas, core: core):
             return .any(of: schemas, core: core.with(title: title))
-        case .not(let schema, core: let core):
+        case let .not(schema, core: core):
             return .not(schema, core: core.with(title: title))
         case .reference:
             return self
@@ -62,4 +61,3 @@ extension JSONSchema.CoreContext {
         )
     }
 }
-
