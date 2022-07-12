4de6213d30a4418d3b33278fc66660a8c57f727d
Simon Bohnen <simon.bohnen@tum.de>
2022-07-12 18:51:46 +0200

    don't use http2 by default

--- TestWebService/Sources/TestWebService/TestWebService.swift
+++ TestWebService/Sources/TestWebService/TestWebService.swift
@@ -36,7 +36,7 @@ struct TestWebService: Apodini.WebService {
     var port: Int?
     
     @Option(help: "The TestWebService's HTTPS config. Omit to disable HTTPS, specify 'builtin' to use buitin self-signed certificates, or pass a path to a custom certificate and key.") // swiftlint:disable:this line_length
-    var httpsConfig: HTTPSConfig = .builtinSelfSignedCertificate
+    var httpsConfig: HTTPSConfig = .none
     
     @Option(help: "Endpoint to expose OpenAPI specification")
     var openApiEndpoint: String = "oas"
